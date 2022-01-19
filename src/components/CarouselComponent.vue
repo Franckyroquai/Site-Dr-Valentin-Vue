a0614b91406636f2fc1754b3e950ec6c67ec9f7c
Franckyroquai <franckyroquai@gmail.com>
2022-01-19 15:45:55 +0100

    ajout de CarouselComponent

---
+++ src/components/CarouselComponent.vue
@@ -0,0 +1,47 @@
+<!--Carousel #}-->
+    <template>
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="../assets/Images/Cw-h6U7XgAAJvUy.jpg-large.jpeg" class="d-block w-100" alt="Oiseau sur l'océan">
+          </div>
+          <div class="carousel-item">
+            <img src="../assets/Images/gettyimages-488915752-612x612.jpg" class="d-block w-100" alt="Surfeur">
+          </div>
+          <div class="carousel-item">
+            <img src="../assets/Images/unnamed.jpg" class="d-block w-100" alt="Montagne">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+      </template>
+
+      <script>
+export default {
+  name: 'CarouselComponent', /*
+  le nom du composant
+  dans le html du parent il se "source" de la maniere suivante : <default-component>
+  dans le scripting (JS) du parent il se declare avec DefaultComponent et doit etre importé
+  */
+  props: {
+      variable1: String // type de variable (string,integer,...)
+  },
+  data() {
+      return {
+          variable2: "ceci est une variable du composant (passable aux composants enfants)"
+      }
+  } 
+}
+</script>
