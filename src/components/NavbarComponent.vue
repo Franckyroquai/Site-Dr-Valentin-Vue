a5a642001ff418b925de1142c3e1a5a0981e1e4d
Franckyroquai <franckyroquai@gmail.com>
2022-01-19 15:47:01 +0100

    modification présentation Navbar

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,55 @@
+<template>
+     <!-- Barre de navigation-->
+    <div class="navbar">
+      <a href="">Accueil</a>
+      <a href="">Rencontre</a>
+      <a href="">Qui suis-je ?</a>
+      <a href="">Méthodes</a>
+      <a href="">Points éthiques</a>
+      <a href="">Liens vidéos</a>
+      <a href="">Modalités pratiques et tarifs</a>
+      <a href="">Téléconsultation</a>
+      <a href="">Blog</a>
+      <a href="">Contact</a>
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'NavbarComponent', /*
+  le nom du composant
+  dans le html du parent il se "source" de la maniere suivante : <default-component>
+  dans le scripting (JS) du parent il se declare avec DefaultComponent et doit etre importé
+  */
+  props: {
+      variable1: String // type de variable (string,integer,...)
+  },
+  data() {
+      return {
+          variable2: "ceci est une variable du composant (passable aux composants enfants)"
+      }
+  } 
+}
+</script>
+
+<style>
+    .navbar{ position: relative;
+    top: 0;
+    left: 0;
+    z-index: 10;
+    width: 100%;
+    height: 5em;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    display: flex;
+    justify-content:space-around;
+    align-items: center;
+    border: 3px solid rgb(248, 234, 248);
+    border-radius:10px;
+    padding: 20px;
+    background-color:lightgray;
+}
+</style>
+
+
