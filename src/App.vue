190087b4251d7ca07c32dd20ca1ebfbabc4e4a97
Franckyroquai <franckyroquai@gmail.com>
2022-01-19 14:36:15 +0100

    suppression des components non utilisés

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <template>
+  <HeaderComponent></HeaderComponent>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from './components/HeaderComponent.vue'
 
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    HeaderComponent,
   }
 }
 </script>
 
 <style>
 #app {
+  margin: 0;
+  padding: 0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
