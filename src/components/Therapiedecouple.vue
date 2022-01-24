56773667e761fe0271912379e223db8c6e5a819b
Franckyroquai <franckyroquai@gmail.com>
2022-01-24 16:27:29 +0100

    création article therapie de couple

---
+++ src/components/Therapiedecouple.vue
@@ -0,0 +1,81 @@
+<template>
+    <main>
+        
+        <h1>V.Thérapie de couple</h1>
+        <article>
+
+         <p>« Le bonheur n'est pas quelque chose qui nous arrive, mais une compétence que nous développons. »
+Paulo Coelho
+</p> 
+
+        <p>
+          Le couple est une construction, une organisation, un fonctionnement original, unique. 
+Un bien mystérieux laboratoire en fait, car un couple qui va bien n’est pas un couple sans problème, c’est un couple qui sait traverser les crises et parvient à les transmuter.
+
+        </p>
+        <p>
+          -Si vous ressentez un déséquilibre dans votre vie à deux ou que vous avez le sentiment de ne plus parler la même langue, de ne plus être, l’un et l’autre, sur la même planète 
+        </p>
+        <p>
+          -Si votre couple fait face à des difficultés (famille recomposée, infidélité, sexualité, addiction, argent, conflits, violence …)
+        </p>
+        <p>
+          Si des événements de vie viennent perturber votre relation à tous deux (arrivée /départ des enfants, chômage, décès d’un proche, maladie, retraite…) et que vous ne vous sentez plus à votre place.
+        </p>
+        <p>
+          Il est temps de prendre du recul, car le problème au fond, ne vient pas nécessairement de l'autre...mais plutôt ce qui se passe dans la relation entre vous !
+        </p>
+        <p>
+          La thérapie systémique est là pour vous permettre de voir et vivre les choses autrement, de mieux vous comprendre et différemment surtout, de percevoir le fonctionnement de votre couple et le rôle que chacun de vous y joue, même si c’est « à son corps défendant ». 
+        </p>
+        <p>
+          Le thérapeute ne se place ni en juge, ni en arbitre mais en facilitateur, avec la mission de faire émerger les attentes de chacun, et vous aider à débloquer les situations-problème. 
+        </p>
+        <p>
+          Vous saurez répondre alors à l’éventuelle question cruciale, « j’arrête ou je continue », soit pour vous re-choisir en retrouvant le chemin de votre cœur, soit pour décider de vous séparer, mais sainement, ce qui est indispensable d’ailleurs pour l’équilibre de vos enfants, si vous en avez.
+        </p>
+        <p>
+          La thérapie de couple ne s’adresse pas qu’aux couples qui sont au bord du gouffre mais à tous ceux qui veulent se donner les moyens de vivre une belle relation.
+        </p>
+        <p>
+            Si vous arrivez à considérer les moments de crises comme un tremplin, vous serez sur la bonne voie pour retrouver confiance et complicité entre vous car il est là, le ciment de votre alliance.
+        </p>
+        <p>
+            L’aventure est certainement pleine de surprises et de rebondissements, j’en conviens.  
+        </p>
+        <p>
+              Mais c’est bien dans cet entre-deux, que je vous propose de vous accompagner : pour écouter vos difficultés, pour vous aider aussi à reconnaître les ressources et compétences qui vous portent et viendront redonner du « peps » à ce que vous avez créé à ce jour.
+        </p>
+        <p>
+            À partir de la thérapie systémique, nous aurons, à disposition, des outils pour vous permettre de transformer l’essai.
+        </p>
+          </article>
+    
+    
+      </main>
+</template>
+
+<script>
+export default {
+  name: 'TherapieDeCoupleComponent', /*
+  le nom du composant
+  dans le html du parent il se "source" de la maniere suivante : <default-component>
+  dans le scripting (JS) du parent il se declare avec DefaultComponent et doit etre importé
+  */
+  props: {
+      variable1: String // type de variable (string,integer,...)
+  },
+  data() {
+      return {
+          variable2: "ceci est une variable du composant (passable aux composants enfants)"
+      }
+  } 
+}
+</script>
+
+<style>
+   main{
+margin: 10px 50px;
+}
+</style>
+
