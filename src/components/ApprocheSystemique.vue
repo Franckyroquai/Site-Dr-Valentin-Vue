0f1b1f0af46fa3eb7ba930f6aea4c23c33526470
Franckyroquai <franckyroquai@gmail.com>
2022-01-24 16:19:19 +0100

    création de l'article Approche systémique

---
+++ src/components/ApprocheSystemique.vue
@@ -0,0 +1,64 @@
+<template>
+    <main>
+        
+        <article>
+
+         <p>L’approche systémique  qui vient de l’école de Palo Alto , s’appuie sur le postulat que l’individu est influencé dans sa façon d’être, par le contexte dans lequel il vit ,ce qui est vécu par l'un a forcément des répercussions sur les autres. Toute la puissance de cette approche consiste à débusquer les blocages enfouis au sein du système et retisser les liens endommagés.</p> 
+
+        <p>
+          La thérapie systémique familiale ou de couple est très pragmatique et ancrée dans le présent, dans la mesure où elle étudie les actions et réactions de chacun des membres de la famille pendant les séances. Car même si le problème trouve souvent ses sources dans des événements passés, elle part du postulat que seul le présent détient la solution. 
+        </p>
+        <p>
+          Pour le thérapeute systémicien, il est important de comprendre, en premier lieu, comment la souffrance s’est installée et identifier la fonction du symptôme dans le système : que permet-il, qu’empêche-t-il… ?
+        </p>
+        <p>
+          Un symptôme peut servir en effet à maintenir une situation en homéostasie, comme, par exemple, l’échec scolaire d’un enfant ou tout comportement hors-norme qui peut monopoliser à ce point l’énergie parentale que les parents en oublient leur mésentente. Cet enfant maintient de la sorte, la stabilité du système.
+        </p>
+        <p>
+          Il aura ainsi pour rôle d’analyser la communication, d’en comprendre les rouages.
+Au contraire de désigner un coupable ou interpréter le problème, il cherchera, ensuite, à modifier le contexte qui le déclenche. 
+
+        </p>
+        <p>
+          Il devra choisir, en fonction, les stratégies thérapeutiques les plus appropriées pour mettre au travail tous les membres du système.
+        </p>
+        <p>
+          Favoriser les échanges, l’expression des émotions, accompagner l’évolution de la communication entre chacun des membres, donnera ainsi au couple ou à l’ensemble du groupe familial l’occasion de dépasser une situation de crise, d’expérimenter de nouvelles manières d’être, de ressentir, d’interagir différemment en somme, de façon à produire des communications allégées donc plus fluides. 
+        </p>
+        <p>
+          Mieux communiquer ensemble permettra également à chacun des membres, de se (ré)approprier une forme d’autonomie psychique, le sentiment d’exister sans être étouffé par son appartenance au système.
+        </p>
+        <p>
+          L’approche systémique familiale fait partie des thérapies brèves. La durée moyenne d’un traitement peut se situer entre 6 et quinze séances, même si, bien sûr, cela est difficile à estimer car adapté à la situation forcément singulière de chaque famille.
+        </p>
+        
+          </article>
+    
+    
+      </main>
+</template>
+
+<script>
+export default {
+  name: 'ApprochesystemiqueComponent', /*
+  le nom du composant
+  dans le html du parent il se "source" de la maniere suivante : <default-component>
+  dans le scripting (JS) du parent il se declare avec DefaultComponent et doit etre importé
+  */
+  props: {
+      variable1: String // type de variable (string,integer,...)
+  },
+  data() {
+      return {
+          variable2: "ceci est une variable du composant (passable aux composants enfants)"
+      }
+  } 
+}
+</script>
+
+<style>
+   main{
+margin: 10px 50px;
+}
+</style>
+
