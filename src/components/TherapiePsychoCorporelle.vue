9b0a717873dcffbb7d943ff1abc0e7f429959785
Franckyroquai <franckyroquai@gmail.com>
2022-01-24 17:26:21 +0100

    mise a jour texte therapie psychocorporelle

--- src/components/TherapiePsychoCorporelle.vue
+++ src/components/TherapiePsychoCorporelle.vue
@@ -15,13 +15,13 @@ C'est par et avec notre corps que nous sommes présents au monde et que nous l'h
 
         </p>
         <p>
-          Si vous vous posez des milliards de questions sur vous, votre vie et que le mental s’emballe… 
+          - Si vous vous posez des milliards de questions sur vous, votre vie et que le mental s’emballe… 
         </p>
         <p>
-          Si vous sentez que vous avez du mal à vous habiter, à accéder à vos ressentis et donc à votre corps….
+          - Si vous sentez que vous avez du mal à vous habiter, à accéder à vos ressentis et donc à votre corps….
         </p>
         <p>
-          Ou qu’il vous joue des tours d’une façon ou d’une autre (maladie, douleurs, tensions, angoisse, blocages…) 
+          - Ou qu’il vous joue des tours d’une façon ou d’une autre (maladie, douleurs, tensions, angoisse, blocages…) 
         </p>
         <p>
          Il est certainement temps de vous mettre à son écoute.
@@ -30,18 +30,31 @@ C'est par et avec notre corps que nous sommes présents au monde et que nous l'h
             Pour en savoir plus
         </h2>
         <p>
-            Toute thérapie passant par le corps s’appuie sur un principe cher à Wilhelm Reich, selon lequel les souvenirs douloureux sont refoulés dans l’inconscient et enregistrés dans le corps. Autrement dit, ce qui n’a pu s’exprimer émotionnellement, s’imprime dans le corps ou finit par s’exprimer par des maux qu’il s’agisse de maux physiques, psychiques ou par un « mal à l’âme » indéfinissable.
+          « L’inconscient est structuré comme un langage » (J. LACAN) … et le corps en est son creuset.
         </p>
         <p>
-          Les pratiques que je suis amenée à vous proposer seront variées et adaptées à vous.
-Il peut s’agir d’exercices de relaxation, de respiration, de postures ou de toucher du corps par exemple, ce toucher thérapeutique si précieux car il vient en point d’appui pour permettre à la personne de se connecter à elle-même, afin de retrouver en premier lieu une base de sécurité.
-
+             Toute thérapie passant par le corps s’appuie sur un principe cher à Wilhelm Reich, selon lequel les souvenirs douloureux sont refoulés dans l’inconscient et enregistrés dans le corps, formant une véritable cuirasse. 
         </p>
         <p>
-          Bien sûr, il n'est pas toujours nécessaire de toucher directement au corps pour travailler avec le corps. L'accueil des ressentis et des émotions ainsi que l’accès à leur contenu affectif permet cette reconnection et un « plus de conscience » sur l’expérience en cours . EMDR ou BRAINSPOTTING entrent dans ce cadre-là, d’autres outils tels que TIPI ou NERTI également. 
+          Les blocages corporels inconscients (tensions musculaires chroniques, zones douloureuses, verrouillage des articulations, altération de la respiration, …) ne sont ni plus ni moins qu’un témoignage de l’histoire de la personne et des défenses qu’elle a édifiées pour s’adapter à son environnement. Autrement dit, ce qui n’a pu s’exprimer émotionnellement, s’imprime dans le corps ou finit par s’exprimer par des maux qu’il s’agisse de maux physiques, psychiques ou par un « mal à l’âme » indéfinissable.
         </p>
         <p>
-          Vous sera parfois proposé, entre les séances, en autonomie donc, quelques pratiques simples telles que la respiration consciente, la cohérence cardiaque, L’EFT…, participant toutes, de ce que j’ai envie d’appeler une écologie de l’Etre, car synonymes de détente, de paix intérieure, d’un prendre soin de soi- donc de vous- « en corps » et toujours.
+          Comprenons que, dès notre conception, notre organisme enregistre chaque expérience vécue. Il garde, au plus profond de ses tissus, les traces des épreuves non digérées qui entravent son développement naturel et ce sous forme de tensions qui deviennent chroniques. Tout cela est inconscient. En dissolvant ces marques du passé, les schémas sclérosés peuvent alors se dénouer en douceur. Le corps s’en trouve apaisé et ressourcé.
+        </p>
+        <p>
+          Permettre à la personne une réappropriation progressive de lui-même et de ses potentialités  afin de lui restituer sa pleine capacité à sentir, à éprouver, aimer et agir, telle paraît bien être la perspective thérapeutique première de cette reconnection intime Psyche-Soma.
+        </p>
+        <p>
+          Les pratiques que je suis amenée à vous proposer seront variées et adaptées aux besoins de chacun(e).
+        </p>
+        <p>
+          Il peut s’agir d’exercices de relaxation, de respiration, de postures ou de toucher du corps par exemple, ce toucher thérapeutique si précieux car il vient en point d’appui pour permettre à la personne de se connecter à elle-même, en premier lieu afin de retrouver une base de sécurité et bien au-delà, apaiser ses parts blessées.
+        </p>
+        <p>
+          Bien sûr, il n'est pas toujours nécessaire de toucher directement au corps pour travailler avec le corps. L'accueil des ressentis et des émotions ainsi que l’accès à leur contenu affectif permet cette reconnection et un « plus de conscience » sur l’expérience en cours afin de contacter votre réalité et votre vérité intérieure. EMDR ou BRAINSPOTTING entrent dans ce cadre-là, d’autres outils tels que TIPI ou NERTI également. 
+        </p>
+        <p>
+          Et, la cerise sur le gâteau, vous sera à coup sûr proposé en accompagnement des séances, en autonomie donc, quelques pratiques simples telles que la respiration consciente, la cohérence cardiaque, L’EFT…, participant toutes, de ce que j’ai envie d’appeler une écologie de l’Etre, car synonymes de bien-être, de paix intérieure, d’un prendre soin de soi- donc de  vous- « en corps » et toujours .
         </p>
           </article>
     
