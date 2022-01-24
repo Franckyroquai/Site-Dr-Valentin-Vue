1410c4b7ec17c51cca0d93dcdcc9591b26635dea
Franckyroquai <franckyroquai@gmail.com>
2022-01-24 17:22:54 +0100

    mise a jour texte Approche systemique

--- src/components/ApprocheSystemique.vue
+++ src/components/ApprocheSystemique.vue
@@ -6,24 +6,32 @@
          <p>L’approche systémique  qui vient de l’école de Palo Alto , s’appuie sur le postulat que l’individu est influencé dans sa façon d’être, par le contexte dans lequel il vit ,ce qui est vécu par l'un a forcément des répercussions sur les autres. Toute la puissance de cette approche consiste à débusquer les blocages enfouis au sein du système et retisser les liens endommagés.</p> 
 
         <p>
+          En thérapie de couple et de famille, l’approche systémique met en avant l’idée que le système concerné s’organise, fonctionne, se régule en fonction des interactions et à partir des règles, explicites et implicites, qui s’y créent. 
+        </p>
+        <p>
           La thérapie systémique familiale ou de couple est très pragmatique et ancrée dans le présent, dans la mesure où elle étudie les actions et réactions de chacun des membres de la famille pendant les séances. Car même si le problème trouve souvent ses sources dans des événements passés, elle part du postulat que seul le présent détient la solution. 
         </p>
         <p>
-          Pour le thérapeute systémicien, il est important de comprendre, en premier lieu, comment la souffrance s’est installée et identifier la fonction du symptôme dans le système : que permet-il, qu’empêche-t-il… ?
+         Pour le thérapeute systémicien, il est important de comprendre, en premier lieu, comment la souffrance s’est installée.
         </p>
         <p>
-          Un symptôme peut servir en effet à maintenir une situation en homéostasie, comme, par exemple, l’échec scolaire d’un enfant ou tout comportement hors-norme qui peut monopoliser à ce point l’énergie parentale que les parents en oublient leur mésentente. Cet enfant maintient de la sorte, la stabilité du système.
-        </p>
-        <p>
-          Il aura ainsi pour rôle d’analyser la communication, d’en comprendre les rouages.
-Au contraire de désigner un coupable ou interpréter le problème, il cherchera, ensuite, à modifier le contexte qui le déclenche. 
+          Un symptôme peut servir à maintenir une situation en homéostasie, c’est-à-dire une stabilité du couple ou de la famille, comme, par exemple, l’échec scolaire d’un l’enfant ou tout comportement hors-norme qui peut monopoliser à ce point l’énergie parentale que les parents en oublient leur mésentente. Cet enfant est alors, en quelque sorte, le « porte-parole » d’un malaise au sein de la famille ou du couple et maintient de la sorte, la stabilité du système.
 
         </p>
         <p>
-          Il devra choisir, en fonction, les stratégies thérapeutiques les plus appropriées pour mettre au travail tous les membres du système.
+          Le thérapeute devra donc identifier la fonction du symptôme dans le système : que permet-il, qu’empêche-t-il… ?
         </p>
         <p>
-          Favoriser les échanges, l’expression des émotions, accompagner l’évolution de la communication entre chacun des membres, donnera ainsi au couple ou à l’ensemble du groupe familial l’occasion de dépasser une situation de crise, d’expérimenter de nouvelles manières d’être, de ressentir, d’interagir différemment en somme, de façon à produire des communications allégées donc plus fluides. 
+          Il aura ainsi pour rôle d’analyser la communication, d’en comprendre les rouages.
+        </p>
+        <p>
+          Au contraire de désigner un coupable ou interpréter le problème, il cherchera, ensuite, à modifier le contexte qui le déclenche. 
+        </p>
+        <p>
+          Il devra choisir, en fonction, les stratégies thérapeutiques les plus appropriées pour mettre au travail tous les membres du système, réguler ses dysfonctionnements adaptatifs afin de susciter de nouvelles ressources et d’ouvrir de nouvelles perspectives.
+        </p>
+        <p>
+           Favoriser les échanges, l’expression des émotions, accompagner l’évolution de la communication entre chacun des membres, donnera ainsi au couple ou à l’ensemble du groupe familial l’occasion de dépasser une situation de crise, d’expérimenter de nouvelles manières d’être, de ressentir, d’interagir différemment en somme, de façon à remettre en marche le processus évolutif et produire des communications allégées donc plus fluides. 
         </p>
         <p>
           Mieux communiquer ensemble permettra également à chacun des membres, de se (ré)approprier une forme d’autonomie psychique, le sentiment d’exister sans être étouffé par son appartenance au système.
@@ -31,7 +39,6 @@ Au contraire de désigner un coupable ou interpréter le problème, il cherchera
         <p>
           L’approche systémique familiale fait partie des thérapies brèves. La durée moyenne d’un traitement peut se situer entre 6 et quinze séances, même si, bien sûr, cela est difficile à estimer car adapté à la situation forcément singulière de chaque famille.
         </p>
-        
           </article>
     
     
