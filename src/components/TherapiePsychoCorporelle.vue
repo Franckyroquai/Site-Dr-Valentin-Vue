09910e4aeb8c071c9671d4b1c7865683d9b1c926
Franckyroquai <franckyroquai@gmail.com>
2022-01-24 16:27:02 +0100

    création article therapie psychocorporelle

---
+++ src/components/TherapiePsychoCorporelle.vue
@@ -0,0 +1,75 @@
+<template>
+    <main>
+        
+        <h1>La thérapie psycho-corporelle</h1>
+        <h2>
+            Une approche corporelle douce 
+        </h2>
+        <article>
+
+         <p> Pour un « CORPS ACCORD »</p> 
+
+        <p>
+          Notre corps est une mémoire vivante qui porte en lui les traces de notre vécu, de notre histoire émotionnelle et affective depuis notre conception, de notre héritage familial.
+C'est par et avec notre corps que nous sommes présents au monde et que nous l'habitons. C’est par notre corps que nous agissons, c’est dans et par notre corps que nous souffrons.
+
+        </p>
+        <p>
+          Si vous vous posez des milliards de questions sur vous, votre vie et que le mental s’emballe… 
+        </p>
+        <p>
+          Si vous sentez que vous avez du mal à vous habiter, à accéder à vos ressentis et donc à votre corps….
+        </p>
+        <p>
+          Ou qu’il vous joue des tours d’une façon ou d’une autre (maladie, douleurs, tensions, angoisse, blocages…) 
+        </p>
+        <p>
+         Il est certainement temps de vous mettre à son écoute.
+        </p>
+        <h2>
+            Pour en savoir plus
+        </h2>
+        <p>
+            Toute thérapie passant par le corps s’appuie sur un principe cher à Wilhelm Reich, selon lequel les souvenirs douloureux sont refoulés dans l’inconscient et enregistrés dans le corps. Autrement dit, ce qui n’a pu s’exprimer émotionnellement, s’imprime dans le corps ou finit par s’exprimer par des maux qu’il s’agisse de maux physiques, psychiques ou par un « mal à l’âme » indéfinissable.
+        </p>
+        <p>
+          Les pratiques que je suis amenée à vous proposer seront variées et adaptées à vous.
+Il peut s’agir d’exercices de relaxation, de respiration, de postures ou de toucher du corps par exemple, ce toucher thérapeutique si précieux car il vient en point d’appui pour permettre à la personne de se connecter à elle-même, afin de retrouver en premier lieu une base de sécurité.
+
+        </p>
+        <p>
+          Bien sûr, il n'est pas toujours nécessaire de toucher directement au corps pour travailler avec le corps. L'accueil des ressentis et des émotions ainsi que l’accès à leur contenu affectif permet cette reconnection et un « plus de conscience » sur l’expérience en cours . EMDR ou BRAINSPOTTING entrent dans ce cadre-là, d’autres outils tels que TIPI ou NERTI également. 
+        </p>
+        <p>
+          Vous sera parfois proposé, entre les séances, en autonomie donc, quelques pratiques simples telles que la respiration consciente, la cohérence cardiaque, L’EFT…, participant toutes, de ce que j’ai envie d’appeler une écologie de l’Etre, car synonymes de détente, de paix intérieure, d’un prendre soin de soi- donc de vous- « en corps » et toujours.
+        </p>
+          </article>
+    
+    
+      </main>
+</template>
+
+<script>
+export default {
+  name: 'TherapiePsychoCorporelleComponent', /*
+  le nom du composant
+  dans le html du parent il se "source" de la maniere suivante : <default-component>
+  dans le scripting (JS) du parent il se declare avec DefaultComponent et doit etre importé
+  */
+  props: {
+      variable1: String // type de variable (string,integer,...)
+  },
+  data() {
+      return {
+          variable2: "ceci est une variable du composant (passable aux composants enfants)"
+      }
+  } 
+}
+</script>
+
+<style>
+   main{
+margin: 10px 50px;
+}
+</style>
+
