4fd605320db06056f4d21692d0b115115d3d8c8a
Franckyroquai <franckyroquai@gmail.com>
2022-01-24 16:20:10 +0100

    création de l'article A propos

---
+++ src/components/Apropos.vue
@@ -0,0 +1,81 @@
+<template>
+    <main>
+        
+        <h1>Bonjour et bienvenue à vous, qui pouvez légitimement avoir quelques interrogations sur mon parcours et ma pratique. </h1>
+        <article>
+
+         <p>Psychologue clinicienne depuis bientôt 30 ans, j’ai goûté à mes débuts, à la profession de psychomotricienne. Cette formation initiale, déjà très engagée à l’époque dans la mouvance psychanalytique, fut un ancrage précieux à ces premières racines qui m’ont attirées en terre freudienne puis lacanienne, curiosité oblige, et enfin jungienne. L’apport des notions d’Archétype et d’Inconscient collectif de Carl Gustav Jung furent, pour moi, une révélation. 
+J’étais prête, dès lors, à m’ouvrir aux apports de la Physique Quantique et avec elle, à un mode de pensée révolutionnaire à l’époque.
+</p> 
+
+        <p>
+          Le corps en tant que creuset, révélateur et attracteur de l’Inconscient, cette porte-là s’est grande ouverte avec une formation de thérapeute, en parallèle à ma formation de psychologue clinicienne, me donnant accès à bien des méthodes psychocorporelles y compris en relaxation.
+        </p>
+        <p>
+          Elle n’a fait que s’agrandir depuis, avec de nouvelles pratiques psycho-neurophysiologiques soutenues par les progrès de l’imagerie cérébrale. L’EMDR et plus récemment le Brainspotting, toutes deux abordant plus directement le lien cerveau-corps-esprit, m’ont plongé radicalement dans les notions de programme de mémoires dysfonctionnelles et d’Informations bloquées.
+        </p>
+        <p>
+          Bien sûr, la guérison guidée par cet Inconscient, je la conçois dans l’intégration de tous les champs thérapeutiques parcourus, et qui, par leur complémentarité, sont comme une danse avec l’intelligence du Vivant. 
+        </p>
+        <p>Ma posture interne, au fond, est de me sentir coach dans l’âme tout autant que thérapeute.
+Retirer ce qui bloque, freine ou parasite, bref, empêche d’aller de l’avant, permet de prendre de la hauteur, de renforcer ses points d’appuis et mobiliser ses ressources,son potentiel.
+Cette voie est celle de la psychologie positive qui donne une bouffée d’air vivifiante à la thérapie en considérant « qu’à côté des multiples problèmes de dysfonctionnements individuels et collectifs  s’exprime et se développe toute une vie riche de sens et de potentialités. »
+✨ 
+
+        </p>
+        <p>
+          D ailleurs, une courte intrusion dans le monde du sport de haut niveau dans le passé, m’a permis de toucher de très près aux problématiques liées à l’anxiété de performance et être amenée à proposer un coaching thérapeutique pour toute personne confrontée à un challenge (artiste, sportif…), en préparation de concours ou examen…
+Se sentir bloqué(e) dans des situations d’échec, en impossibilité d’assumer sa place voire même à se sentir légitime dans la réussite, le BRAINSPOTTING est un outil des plus appropriés pour switcher sur ces situations-là.
+
+        </p>
+        <p>
+         Sur ce parcours qui est le mien, j’ai rencontré bien sûr des personnes inspirantes :
+        </p>
+        <p>
+          -Ma psychanalyste, certes lacanienne, mais qui a su m’insuffler bien plus que du Lacan.
+        </p>
+        <p>
+         -Mon très cher collègue Jean-Louis, psychanalyste et bouddhiste jusqu’au fond de l’âme, adepte d’un amour de l’autre inconditionnel.
+        </p>
+        <p>
+          -  Mes formateurs EMDR et Brainspotting , tous champions d’une pensée intégrative qui concilie ce qui peut sembler parfois de l’ordre de l’inconciliable.
+        </p>
+        <p>
+            - et enfin mon formateur et Maître, à qui je dois d’en être où j’en suis aujourd’hui et qui a su donner à ma pratique et surtout à ma Vie un Souffle qui œuvre toujours en moi, souvent à mon insu et inlassablement me pousse vers tous les champs du possible.  
+        </p>
+        <p>
+            Où trouvons-vous une limite à notre Etre, puisque Tout est en nous.  Et si nous acceptons de plonger dans ce vaste Océan de la conscience, nous ne pouvons que vivre un élargissement de notre être. 
+        </p>
+        <p>
+            Djalâl Al-Dîn Rûmi ne disait-il pas" Vous n'êtes pas une goutte dans l'océan. Vous êtes l'océan tout entier dans une goutte d'eau. "
+        </p>
+          </article>
+    
+    
+      </main>
+</template>
+
+<script>
+export default {
+  name: 'AproposComponent', /*
+  le nom du composant
+  dans le html du parent il se "source" de la maniere suivante : <default-component>
+  dans le scripting (JS) du parent il se declare avec DefaultComponent et doit etre importé
+  */
+  props: {
+      variable1: String // type de variable (string,integer,...)
+  },
+  data() {
+      return {
+          variable2: "ceci est une variable du composant (passable aux composants enfants)"
+      }
+  } 
+}
+</script>
+
+<style>
+   main{
+margin: 10px 50px;
+}
+</style>
+
