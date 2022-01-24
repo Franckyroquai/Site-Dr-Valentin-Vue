be88782af806954a7ed9545d4fd74309ae96f399
Franckyroquai <franckyroquai@gmail.com>
2022-01-24 16:23:14 +0100

    création de l'article EMDR

---
+++ src/components/EMDR.vue
@@ -0,0 +1,66 @@
+<template>
+    <main>
+        
+        <h1>QU'EST-CE QUE L'EMDR ?</h1>
+        <article>
+
+         <p>Les initiales EMDR « Eye Movement Desensitization and Reprocessing » signifient  Désensibilisation et Retraitement par les Mouvements Oculaires.
+La thérapie EMDR utilise en effet, une stimulation sensorielle bilatérale alternée (droite-gauche) des lobes du cerveau, par mouvements oculaires bi-alternés, tapotements ou sons alternatifs. 
+</p> 
+
+        <p>
+          Cette thérapie a été découverte en 1987 de manière fortuite et mise au point par Francine SHAPIRO. 
+        </p>
+        <p>
+          Cette psychologue américaine a trouvé ce moyen très simple de stimuler un mécanisme neuropsychologique complexe, présent en chacun de nous, qui permet de retraiter des vécus   traumatiques non digérés à l’origine de divers symptômes, parfois très invalidants. 
+Il peut s’agir de traumatismes tels les violences physiques et psychologiques, les abus sexuels, les accidents graves, les décès, maladies graves, incendies, catastrophes naturelles, les situations de guerre et attentats … Il peut s’agir aussi d’événements de vie difficiles, sources de fortes émotions ou de comportements inadaptés ou excessifs dans la vie quotidienne, une enfance perturbée, les séparations, interruptions de grossesse, deuils, difficultés professionnelles ...) Des symptômes peuvent alors apparaître tels que des troubles anxieux (angoisses, cauchemars…), de l’humeur, des comportements alimentaires (TCA), troubles sexuels, addictions, phobies, maladies chroniques, TOC, douleurs, états dissociatifs …Le processus psychique de traitement activé par cette méthode est un processus conscient qui diminue la charge émotionnelle associée à un souvenir.
+
+        </p>
+        <p>
+          La thérapie EMDR s’adresse à toute personne de tout âge (du bébé à l’adulte) souffrant de perturbations émotionnelles généralement liées à des situations perturbantes émotionnellement ou des traumatismes psychologiques. 
+        </p>
+        <p>
+          Une séance d’EMDR dure 60 minutes environ, parfois plus.
+        </p>
+        <p>
+          Du fait de l’effet puissant de cette thérapie sur le psychisme du patient,des entretiens préliminaires sont en effet nécessaires pour :
+        </p>
+        <p>
+          construire une relation thérapeutique de confiance .
+        </p>
+        <p>
+          identifier  une problématique actuelle susceptible d’être traitée en EMDR, puis les souvenirs traumatiques à l’origine de ces difficultés.
+        </p>
+        <p>
+          et enfin mettre en place des outils psychocorporels de stabilisation émotionnelle qui peuvent être utilisés en cours de séance mais aussi en pratique autonome entre les séances.
+        </p>
+          </article>
+    
+    
+      </main>
+</template>
+
+<script>
+export default {
+  name: 'EMDRComponent', /*
+  le nom du composant
+  dans le html du parent il se "source" de la maniere suivante : <default-component>
+  dans le scripting (JS) du parent il se declare avec DefaultComponent et doit etre importé
+  */
+  props: {
+      variable1: String // type de variable (string,integer,...)
+  },
+  data() {
+      return {
+          variable2: "ceci est une variable du composant (passable aux composants enfants)"
+      }
+  } 
+}
+</script>
+
+<style>
+   main{
+margin: 10px 50px;
+}
+</style>
+
