8dd94d6dbc95f85f94a28695537dcbe4b0d2c335
Franckyroquai <franckyroquai@gmail.com>
2022-01-30 18:06:23 +0100

    mise a jour main.js router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
 
-createApp(App).mount('#app')
+createApp(App).use(Router).mount('#app')
