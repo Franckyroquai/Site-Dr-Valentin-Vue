f668d1f888766e567d2b0556b27dbaa7962bd013
Franckyroquai <franckyroquai@gmail.com>
2022-02-03 21:19:14 +0100

    creation component error 404

---
+++ src/components/Error404Component.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>Erreur 404</h1>
+    <div class="image-404">
+            <img src="../assets/Images/Page-Erreur-404.jpg" class="d-block w-100" alt="Erreur 404">
+    </div>
+    <!-- ici ta structure Html -->
+</template>
+
+<script>
+export default {
+  name: 'Error404Component', /*
+  le nom du composant
+  dans le html du parent il se "source" de la maniere suivante : <error404-component>
+  dans le scripting (JS) du parent il se declare avec Error404Component et doit etre importé
+  */
+  props: {
+      variable1: String // type de variable (string,integer,...)
+  },
+  data() {
+      return {
+          variable2: "ceci est une variable du composant (passable aux composants enfants)"
+      }
+  } 
+}
+</script>
+
+<style>
+    /* ici le css */
+</style>
+
