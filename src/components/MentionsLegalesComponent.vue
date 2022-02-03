062db942f01c56919e409d562ebb50bf6a4e5bd8
Franckyroquai <franckyroquai@gmail.com>
2022-02-03 21:21:04 +0100

    creation component mentions legales

---
+++ src/components/MentionsLegalesComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>Mentions Légales</h1>
+    <!-- ici ta structure Html -->
+</template>
+
+<script>
+export default {
+  name: 'MentionsLegalesComponent', /*
+  le nom du composant
+  dans le html du parent il se "source" de la maniere suivante : <mentionslegales-component>
+  dans le scripting (JS) du parent il se declare avec MentionsLegalesComponent et doit etre importé
+  */
+  props: {
+      variable1: String // type de variable (string,integer,...)
+  },
+  data() {
+      return {
+          variable2: "ceci est une variable du composant (passable aux composants enfants)"
+      }
+  } 
+}
+</script>
+
+<style>
+    /* ici le css */
+</style>
+
