8ffc079f19c6e5264df4845d9e9768b4e80d8ad7
Franckyroquai <franckyroquai@gmail.com>
2022-02-07 21:35:14 +0100

    ajout de liens videos dans le component

--- src/components/LiensVideosComponent.vue
+++ src/components/LiensVideosComponent.vue
@@ -1,5 +1,11 @@
 <template>
-    <h1>Liens Viédos</h1>
+    <h1>Liens Viédos</h1>    
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/87sNeRTYdPA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/omfpbqDMDN4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/weAQzjtxNPE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/PDIJb2emVDY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/EXtIEgOq4Xc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
     <!-- ici ta structure Html -->
 </template>
 
