b3e283dd89ba19a921eff17cc6bc38b4e3e2e89a
Franckyroquai <franckyroquai@gmail.com>
2022-02-12 18:40:15 +0100

    changement de police Georgia

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,8 @@ export default {
   components: {
     HeaderComponent,
     NavbarComponent,
+    // CarouselComponent,
+    // AccueilComponent,
   }
 }
 </script>
@@ -23,9 +25,7 @@ export default {
 #app {
   margin: 0;
   padding: 0;
-  font-family: verdana, Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Georgia;
   text-align: center;
   color: #2c3e50;
   margin-top: 0px;
