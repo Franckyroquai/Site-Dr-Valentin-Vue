1fb3b7b2c880bae0f898049b6e6606bfb03c59e4
Franckyroquai <franckyroquai@gmail.com>
2022-02-12 19:20:14 +0100

    ajout Therapie de l'adolescent dans navbar

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -10,6 +10,7 @@
       <router-link to="/emdr">EMDR</router-link>
       <router-link to="/brainspotting">Brainspotting</router-link>
       <router-link to="/therapie-enfant">Thérapie de l'Enfant</router-link>
+      <router-link to="/therapie-adolescent">Thérapie de l'Adolescent</router-link>
       <router-link to="/therapie-familiale">Thérapie Familiale</router-link>
       <router-link to="/therapie-de-couple">Thérapie de couple</router-link>
       <router-link to="/therapie-psychocorporelle">Thérapie PsychoCorporelle</router-link>
