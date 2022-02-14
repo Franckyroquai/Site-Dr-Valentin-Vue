f8d9fe0fa48bd384732c357bec4a330eaad15113
Franckyroquai <franckyroquai@gmail.com>
2022-02-14 21:24:24 +0100

    ajout component therapie adulte

---
+++ src/components/TherapieAdulte.vue
@@ -0,0 +1,84 @@
+<template>
+    <main>
+        
+        <h1>Les adultes / séniors</h1>
+        
+        <article>
+        <h2>
+            Vous vous sentez en difficulté dans votre vie personnelle, affective, au niveau professionnel, face à un challenge ? 
+        </h2>
+        <p>
+            Vous subissez : 
+        </p>
+        <ul>
+            <li>Un évènement traumatique, une agression, un accident…</li>
+            <li>Une maladie chronique, psychosomatique ...</li>
+            <li>Une séparation, la perte d’un proche, un veuvage,  l’isolement de la retraite</li>
+            <li>Une situation douloureuse, une tristesse excessive, des accès de colère incontrôlables </li>
+            <li>Des difficultés professionnelles, burn-out, démission, harcèlement…</li>
+            <li>Une dépression, le sentiment d’être  inutile</li>
+            <li>De l’anxiété, angoisses, phobies, attaques de panique…</li>
+            <li>Un manque de confiance en vous, des doutes existentiels…</li>
+            <li>Une addiction (drogue, alcool, jeux, achat, sexe…)</li>
+            <li>Des difficultés relationnelles… </li>
+        </ul>
+        <p>
+           Lors de votre préparation aux concours, examens, entretiens, challenges divers, vous aimeriez :
+        </p>
+        <ul>
+            <li>Augmenter la confiance et l’estime de vous-même</li>
+            <li>Apaiser le stress</li>
+            <li>Développer votre concentration </li>
+            <li>Retrouver vos pleines capacités après une blessure</li>
+            <li>Dépasser la peur de gagner ou de perdre…</li>
+        </ul>
+        <p>
+           En vous lançant dans ce voyage thérapeutique vous vous donnez l’opportunité de : 
+        </p>
+        <ul>
+            <li>Prendre conscience de vos freins, blocages, conditionnements, croyances limitantes.</li>
+            <li>Changer de regard sur les situations. </li>
+            <li>Vous sentir de plus en plus aligné(e) avec ce que vous êtes profondément.</li>
+        </ul>
+        <p>
+          Beau challenge en perspective car dans cette transformation, c’est à votre nature profonde que vous accédez et c’est votre place dans le monde que vous redessinez.
+        </p>
+        <p>
+           Beau challenge en perspective car dans cette transformation, c’est à votre nature profonde que vous accédez et c’est votre place dans le monde que vous redessinez.
+        </p>
+        <p>
+           La 1ère séance sera l'occasion d’exprimer vos attentes et préfigurer votre destination.
+        </p>
+        <p>
+           Votre voyage peut alors débuter, faisons un bout de chemin ensemble.
+        </p>
+        </article>
+    
+    
+      </main>
+</template>
+
+<script>
+export default {
+  name: 'TherapieAdulteComponent', /*
+  le nom du composant
+  dans le html du parent il se "source" de la maniere suivante : <default-component>
+  dans le scripting (JS) du parent il se declare avec DefaultComponent et doit etre importé
+  */
+  props: {
+      variable1: String // type de variable (string,integer,...)
+  },
+  data() {
+      return {
+          variable2: "ceci est une variable du composant (passable aux composants enfants)"
+      }
+  } 
+}
+</script>
+
+<style>
+   main{
+margin: 10px 50px;
+}
+</style>
+
