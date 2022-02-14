64a28ab10634f38347ac0e2d84c6d08d4b4c4755
Franckyroquai <franckyroquai@gmail.com>
2022-02-14 21:26:21 +0100

    MAJ component Accueil

--- src/components/AccueilComponent.vue
+++ src/components/AccueilComponent.vue
@@ -1,39 +1,36 @@
 <template>
     <main>
       <carousel-component></carousel-component>
-        <p>« J’ouvrirai une école de vie intérieure, et j’écrirai sur la porte : école d’art. » Max Jacob</p>
-        <h1>V. Valentin Psychologue Conseil</h1>
+        
         <article>
+        <h1>
+          Bienvenue
+        </h1>
+        <h2>
+          « J’ouvrirai une école de vie intérieure, et j’écrirai sur la porte : école d’art. »<br>Max Jacob
+        </h2>
+        <p>
+          Nous sommes amenés dans notre vie, à traverser des périodes houleuses, des expériences compliquées, douloureuses. Nous pouvons, dans ces moments-là, nous sentir fragilisé(e). Et si les obstacles s’accumulent ou reviennent de manière récurrente, tout cela peut nous sembler subitement insoluble ou nous donner l’impression de ne plus arriver à avancer.
+        </p> 
 
-         <p>Nous sommes amenés dans notre vie, à traverser des périodes houleuses, des expériences compliquées, douloureuses. Nous pouvons, dans ces moments-là, nous sentir fragilisé(e). Et si les obstacles s’accumulent ou reviennent de manière récurrente, tout cela peut nous sembler subitement insoluble ou nous donner l’impression de ne plus arriver à avancer.</p> 
-
         <p>
-          Quelques soient les difficultés que vous rencontrez, les questions que vous vous posez, germe peut-être au fond de vous, le besoin de vous sentir écouté, entendu, compris, dans un endroit où tout peut être dit, sans tabou, ni peur d’être jugé(e).
+          Quelques soient les difficultés que vous rencontrez, les questions que vous vous posez, germe peut-être au fond de vous, le désir de transformer votre horizon personnel pour aller vers un mieux-être, ou mieux, vers un plus d’Etre.
         </p>
         <p>
-          C’est là que l’intervention d’un professionnel peut être bénéfique. 
+         « Un voyage de mille lieux commence toujours par un premier pas »<br>Lao Tseu
         </p>
         <p>
-          Grâce à son regard extérieur, il vous facilitera le pas de côté nécessaire au changement. Vous apprendrez alors à vous voir au-delà des limites que vous vous imposez.
+          Aussi je vous propose de faire le point avec vous.
         </p>
         <p>
-          Je vous propose de faire le point avec vous. A votre rythme, selon vos besoins et vos attentes, il pourra alors vous être proposé :
+         Mes consultations sont ouvertes à tous, enfants, adolescents, adultes, séniors, ainsi que couples et familles. 
         </p>
         <p>
-          -une aide ponctuelle face à une situation particulière, peut -être pour prendre de la hauteur et accéder à quelques clés au passage.
+         Je pratique essentiellement la psychothérapie EMDR, le Brainspotting, diverses approches en thérapie psychocorporelle et la pratique Systémique.
         </p>
-        <p>
-          -ou un accompagnement sur une durée plus longue, voire une thérapie si vous envisagez d’aller plus loin.
-        </p>
-        <p>
-          Mes consultations sont ouvertes à tous, enfants, adolescents, adultes, séniors, couples et familles.
-        </p>
-        <p>
-          Si vous décidez d’ouvrir cette page, c’est que le moment est peut-être venu d’OSER,  oser rêver votre vie pour oser la vivre, et pour le meilleur et sans le pire, arriver à  vous y réaliser .
-        </p>
-        <p>
-          « Le possible est juste un tout petit peu après l’impossible, un seul pas peut suffire pour le rencontrer » J. Salomé
-        </p>
+        <h2>
+         « Le possible est juste un tout petit peu après l’impossible, un seul pas peut suffire pour le rencontrer »<br>J. Salomé
+        </h2>
           </article>
     
     
