7abe0f3b5f2db7369db3835a3ce92933f6247dde
Franckyroquai <franckyroquai@gmail.com>
2022-02-21 14:41:41 +0100

    changement de presentation la navbar

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,9 +1,66 @@
 <template>
      <!-- Barre de navigation-->
-    <div class="navbar">
-      <!-- <a href="">Accueil</a> -->
+
+    <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light style=background-color: #e3f2fd;">
+  <div class="container-fluid">
+    <router-link to="/" class="navbar-brand">V. Valentin</router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link active" aria-current="page">Accueil</router-link>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Qui suis-je ?
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><router-link to="/about" class="dropdown-item">Mon parcours, ma pratique</router-link></li>
+            <li><router-link to="/points-ethiques" class="dropdown-item">Points Ethiques</router-link></li>
+            <li><router-link to="/diplomes" class="dropdown-item">Diplômes et Certifications</router-link></li>
+            <li><router-link to="/liens-videos" class="dropdown-item">Liens Vidéos</router-link></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Méthodes
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><router-link to="/approche-therapeutique" class="dropdown-item">Approche Thérapeutique</router-link></li>
+            <li><router-link to="/approche-systemique" class="dropdown-item">Approche Systémique</router-link></li>
+            <li><router-link to="/emdr" class="dropdown-item">EMDR</router-link></li>
+            <li><router-link to="/brainspotting" class="dropdown-item">Brainspotting</router-link></li>
+            <li><router-link to="/therapie-psychocorporelle" class="dropdown-item">Thérapie PsychoCorporelle</router-link></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Thérapies
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><router-link to="/therapie-enfant" class="dropdown-item">Thérapie de l'Enfant</router-link></li>
+            <li><router-link to="/therapie-adolescent" class="dropdown-item">Thérapie de l'Adolescent</router-link></li>
+            <li><router-link to="/therapie-adulte" class="dropdown-item">Thérapie de l'Adulte</router-link></li>
+            <li><router-link to="/therapie-de-couple" class="dropdown-item">Thérapie de couple</router-link></li>
+            <li><router-link to="/therapie-familiale" class="dropdown-item">Thérapie Familiale</router-link></li>
+          </ul>
+        </li>
+      
+        <li class="nav-item">
+          <router-link to="/modalites-pratiques" class="nav-link">Modalités Pratiques</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/contact" class="nav-link">Contact</router-link>
+        </li>
+        </ul>
+    </div>
+  </div>
+</nav>
+     
+    <!-- <div class="navbar">
       <router-link to="/">Accueil</router-link>
-      <!-- <a href="">Rencontre</a> -->
       <router-link to="/about">A Propos</router-link>
       <router-link to="/approche-systemique">Approche Systémique</router-link>
       <router-link to="/approche-therapeutique">Approche Thérapeutique</router-link>
@@ -22,8 +79,8 @@
       <router-link to="/contact">Contact</router-link>
       
 
-      <!-- <link-component Link="wwww.google.fr" linkName="le nom du lien"></link-component> -->
-   </div>
+      <link-component Link="wwww.google.fr" linkName="le nom du lien"></link-component>
+   </div> -->
    <!-- <router-view></router-view> -->
 </template>
 
@@ -51,12 +108,12 @@ export default {
 </script>
 
 <style>
-    .navbar{ position: relative;
+    /* .navbar{ position: relative;
     top: 0;
     left: 0;
     z-index: 10;
     width: 100%;
-    height: 5em;
+    height: 6em;
     display: flex;
     justify-content: space-around;
     align-items: center;
@@ -66,8 +123,14 @@ export default {
     border: 3px solid rgb(248, 234, 248);
     border-radius:10px;
     padding: 20px;
-    background-color:lightgray;
-}
+    background-color:rgb(211, 211, 211);
+} */
+/* .navbar-nav > li{
+  
+  margin-left:25px;
+  margin-right:25px;
+} */
+
 </style>
 
 
