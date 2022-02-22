febfcc7e505ff2e1d03aa1b839100ae91471afc4
Franckyroquai <franckyroquai@gmail.com>
2022-02-22 21:14:39 +0100

    Correction erreur dans la phrase

--- src/components/Error404Component.vue
+++ src/components/Error404Component.vue
@@ -1,7 +1,7 @@
 <template>
-    <h1>Erreur 404</h1>
+    <h1>Cette page n'est pas attribuée...</h1>
     <div class="image-404">
-            <img src="../assets/Images/404ariane.png" class="d-block w-100" alt="Erreur 404">
+            <img src="../assets/Images/404ariane1.jpg" class="d-block w-100" alt="Erreur 404 image de fil d'ariane coupé">
     </div>
     <!-- ici ta structure Html -->
 </template>
@@ -26,5 +26,11 @@ export default {
 
 <style>
     /* ici le css */
+    .image-404{
+         height:100vh;
+    background-position: center;
+    background-size: cover;
+    }
+
 </style>
 
