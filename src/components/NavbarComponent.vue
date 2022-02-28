15254b045b0f0f74d81e481bb7c425169a92aaaf
Franckyroquai <franckyroquai@gmail.com>
2022-02-28 17:56:15 +0100

    ajout du style (droite et ombre)

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,14 +1,15 @@
 <template>
      <!-- Barre de navigation-->
 
-    <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light style=background-color: #e3f2fd;">
+    <nav class="navbar sticky-top shadow-sm navbar-expand-lg navbar-light bg-light style=background-color: #e3f2fd;">
   <div class="container-fluid">
     <router-link to="/" class="navbar-brand">V. Valentin</router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <!-- pour aligner les menus à gauche, remplacer ms-auto par me-auto, centrer mx-auto -->
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <router-link to="/" class="nav-link active" aria-current="page">Accueil</router-link>
         </li>
