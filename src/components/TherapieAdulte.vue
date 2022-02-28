3efb25dea6c61f4d2e73f17f5eead575dcd3476c
Franckyroquai <franckyroquai@gmail.com>
2022-02-28 17:55:04 +0100

    ajout du style

--- src/components/TherapieAdulte.vue
+++ src/components/TherapieAdulte.vue
@@ -1,15 +1,17 @@
 <template>
     <main>
         
-        <h1>Les adultes / séniors</h1>
+        <h1 class="i">La thérapie pour adultes / séniors</h1>
+        <br>
         
         <article>
-        <h2>
+        <h2 class="i">
             Vous vous sentez en difficulté dans votre vie personnelle, affective, au niveau professionnel, face à un challenge ? 
         </h2>
-        <p>
+        <br>
+        <h3>
             Vous subissez : 
-        </p>
+        </h3>
         <ul>
             <li>Un évènement traumatique, une agression, un accident…</li>
             <li>Une maladie chronique, psychosomatique ...</li>
@@ -22,9 +24,9 @@
             <li>Une addiction (drogue, alcool, jeux, achat, sexe…)</li>
             <li>Des difficultés relationnelles… </li>
         </ul>
-        <p>
+        <h3>
            Lors de votre préparation aux concours, examens, entretiens, challenges divers, vous aimeriez :
-        </p>
+        </h3>
         <ul>
             <li>Augmenter la confiance et l’estime de vous-même</li>
             <li>Apaiser le stress</li>
@@ -32,14 +34,15 @@
             <li>Retrouver vos pleines capacités après une blessure</li>
             <li>Dépasser la peur de gagner ou de perdre…</li>
         </ul>
-        <p>
+        <h3>
            En vous lançant dans ce voyage thérapeutique vous vous donnez l’opportunité de : 
-        </p>
+        </h3>
         <ul>
             <li>Prendre conscience de vos freins, blocages, conditionnements, croyances limitantes.</li>
             <li>Changer de regard sur les situations. </li>
             <li>Vous sentir de plus en plus aligné(e) avec ce que vous êtes profondément.</li>
         </ul>
+        <br>
         <p>
           Beau challenge en perspective car dans cette transformation, c’est à votre nature profonde que vous accédez et c’est votre place dans le monde que vous redessinez.
         </p>
