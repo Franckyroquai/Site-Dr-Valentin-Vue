eb105843462e6b0845e5ed490e168c8365f78710
Franckyroquai <franckyroquai@gmail.com>
2022-03-05 20:08:03 +0100

    nettoyage code

--- src/components/MentionsLegalesComponent.vue
+++ src/components/MentionsLegalesComponent.vue
@@ -11,11 +11,10 @@ export default {
   dans le scripting (JS) du parent il se declare avec MentionsLegalesComponent et doit etre importé
   */
   props: {
-      variable1: String // type de variable (string,integer,...)
+
   },
   data() {
       return {
-          variable2: "ceci est une variable du composant (passable aux composants enfants)"
       }
   } 
 }
