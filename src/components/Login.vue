1870ac88231c1b5fcc2845768c5f52e0f44c4682
Franckyroquai <franckyroquai@gmail.com>
2022-03-05 20:16:05 +0100

    nettoyage code

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,43 +1,45 @@
 <template>
-    <h1>Connexion</h1>
-    <form>
-  <div class="form-group">
-    <label for="exampleInputName1">Nom</label>
-    <input type="text" class="form-control" id="exampleInputName1" aria-describedby="nameHelp" placeholder="Votre nom">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Adresse Email</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Votre email">
-    <small id="emailHelp" class="form-text text-muted">Nous ne communiquons pas votre adresse email à des tiers.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Mot de passe</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Votre mot de passe">
-  </div>
-  <button type="submit" class="btn btn-primary">Envoyer</button>
-</form>
-    <!-- ici ta structure Html -->
+  <h1>Connexion</h1>
+  <form>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Adresse Email</label>
+      <input
+        type="email"
+        class="form-control"
+        id="exampleInputEmail1"
+        aria-describedby="emailHelp"
+        placeholder="Votre email"
+      />
+      <small id="emailHelp" class="form-text text-muted"
+        >Nous ne communiquons pas votre adresse email à des tiers.</small
+      >
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Mot de passe</label>
+      <input
+        type="password"
+        class="form-control"
+        id="exampleInputPassword1"
+        placeholder="Votre mot de passe"
+      />
+    </div>
+    <button type="submit" class="btn btn-primary">Envoyer</button>
+  </form>
+  <router-link to="/register">Pas encore inscrit ? cliquez ici !</router-link>
+  <!-- ici ta structure Html -->
 </template>
 
 <script>
 export default {
-  name: 'LoginComponent', /*
-  le nom du composant
-  dans le html du parent il se "source" de la maniere suivante : <login-component>
-  dans le scripting (JS) du parent il se declare avec LoginComponent et doit etre importé
-  */
-  props: {
-      variable1: String // type de variable (string,integer,...)
-  },
+  name: "LoginComponent",
   data() {
-      return {
-          variable2: "ceci est une variable du composant (passable aux composants enfants)"
-      }
-  } 
-}
+    return {
+    };
+  },
+};
 </script>
 
 <style>
-    /* ici le css */
+/* ici le css */
 </style>
 
