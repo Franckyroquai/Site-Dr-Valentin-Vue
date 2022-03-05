8eb088530858dd3d6fa633912c71bb495e48fe71
Franckyroquai <franckyroquai@gmail.com>
2022-03-05 20:15:23 +0100

    creation composant register avec form qui communique

--- src/components/RegisterComponent.vue
+++ src/components/RegisterComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>Inscription</h1>
+  <!-- <h1>Inscription</h1>
     <form>
   <div class="form-group">
     <label for="exampleInputName1">Nom</label>
@@ -19,29 +19,134 @@
     <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Confirmer votre mot de passe">
   </div>
   <button type="submit" class="btn btn-primary">Envoyer</button>
-</form>
-    <!-- ici ta structure Html -->
+</form> -->
+
+  <div class="root">
+    <h2>Create an Account</h2>
+    <p>
+      <input type="text" placeholder="Email" v-model="state.email" />
+      <span v-if="v$.email.$error">
+        {{ v$.email.$errors[0].$message }}
+      </span>
+    </p>
+    <p>
+      <input type="password" placeholder="Password" v-model="state.password.value" />
+      <span v-if="v$.password.value.$error">
+        {{ v$.password.value.$errors[0].$message }}
+      </span>
+    </p>
+    <p>
+      <input
+        type="password"
+        placeholder="Confirm Password"
+        v-model="state.password.confirm"
+      />
+      <span v-if="v$.password.confirm.$error">
+        {{ v$.password.confirm.$errors[0].$message }}
+      </span>
+    </p>
+    <button @click="submitForm">Submit</button>
+  </div>
 </template>
 
 <script>
+import useValidate from "@vuelidate/core";
+import { reactive, computed } from "vue";
+import { required, email, minLength, sameAs } from "@vuelidate/validators";
+import axios from "axios";
+
 export default {
-  name: 'RegisterComponent', /*
-  le nom du composant
-  dans le html du parent il se "source" de la maniere suivante : <register-component>
-  dans le scripting (JS) du parent il se declare avec RegisterComponent et doit etre importé
-  */
-  props: {
-      variable1: String // type de variable (string,integer,...)
+  name: "RegisterComponent",
+  setup() {
+    const state = reactive({
+      email: "",
+      password: {
+        value: "",
+        confirm: "",
+      },
+    });
+    const rules = computed(() => {
+      return {
+      email: { required, email },
+      password: {
+        value: { required, minLength: minLength(6) },
+        confirm: { required, sameAs: sameAs(state.password.value) },
+      }
+      }});
+
+      const v$ = useValidate(rules, state)
+      
+      return { state, v$ };
   },
   data() {
-      return {
-          variable2: "ceci est une variable du composant (passable aux composants enfants)"
+    return {};
+  },
+  methods: {
+    async submitForm() {
+      this.v$.$validate(); // vérifie les inputs
+      if (!this.v$.$error) {
+        try {
+
+        const httpResponse = await axios.post(
+          "http://127.0.0.1:3000/signup", 
+          {
+            email: this.state.email, 
+            password: this.state.password.value
+          })
+          console.log(httpResponse.status);
+          if (httpResponse.status === 200) {
+            alert("vous avez bien ete enregistre.\nVous allez etre redirige vers la page de login")
+            this.$router.push("/login");
+          } 
+        } catch (error) {
+          console.error(error);
+          alert ("does not compute, Error, Error, ERRR.&%^&$");
+        }
+      } else {
+        console.info("password: ", this.state.password.value);
+        console.info("confirm : ", this.state.password.confirm);
+        this.v$.$errors.forEach(error => console.warn(error.$message));
+        console.error("Form failed validation");
       }
-  } 
-}
+    },
+  },
+  validations() {
+    return {
+      email: { required },
+      password: {
+        value: { required },
+        confirm: { required },
+      },
+    };
+  },
+};
 </script>
 
 <style>
-    /* ici le css */
-</style>
+.root {
+  width: 400px;
+  margin: 0 auto;
+  background-color: #fff;
+  padding: 30px;
+  margin-top: 100px;
+  border-radius: 20px;
+}
 
+input {
+  border: none;
+  outline: none;
+  border-bottom: 1px solid #ddd;
+  font-size: 1em;
+  padding: 5px 0;
+  margin: 10px 0 5px 0;
+  width: 100%;
+}
+
+button {
+  background-color: #3498db;
+  padding: 10px 20px;
+  margin-top: 10px;
+  border: none;
+  color: white;
+}
+</style>
