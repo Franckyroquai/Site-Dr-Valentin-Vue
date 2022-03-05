5a0187e39cd48515634c2009c7488a23e0704ffd
Franckyroquai <franckyroquai@gmail.com>
2022-03-05 20:13:32 +0100

    nettoyage code

--- src/components/Brainspotting.vue
+++ src/components/Brainspotting.vue
@@ -134,11 +134,7 @@
 
 <script>
 export default {
-  name: "BrainspottingComponent" /*
-  le nom du composant
-  dans le html du parent il se "source" de la maniere suivante : <default-component>
-  dans le scripting (JS) du parent il se declare avec DefaultComponent et doit etre importé
-  */,
+  name: "BrainspottingComponent",
   props: {
     variable1: String, // type de variable (string,integer,...)
   },
