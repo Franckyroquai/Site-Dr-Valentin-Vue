e1a6246d1627dbcdfd2f9ef4b456a71c8dcde249
Franckyroquai <franckyroquai@gmail.com>
2022-03-07 15:38:14 +0100

    nettoyage code

--- src/components/Therapiedecouple.vue
+++ src/components/Therapiedecouple.vue
@@ -59,12 +59,17 @@
 
 <script>
 export default {
-  name: 'TherapieDeCoupleComponent',
+  name: 'TherapieDeCoupleComponent', /*
+  le nom du composant
+  dans le html du parent il se "source" de la maniere suivante : <default-component>
+  dans le scripting (JS) du parent il se declare avec DefaultComponent et doit etre importé
+  */
   props: {
-
+      variable1: String // type de variable (string,integer,...)
   },
   data() {
       return {
+          variable2: "ceci est une variable du composant (passable aux composants enfants)"
       }
   } 
 }
