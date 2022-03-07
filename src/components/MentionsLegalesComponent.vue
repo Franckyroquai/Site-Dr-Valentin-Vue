2064b8898a9069008bda1983f74e1653895fffed
Franckyroquai <franckyroquai@gmail.com>
2022-03-07 21:05:39 +0100

    creation texte mentions légales

--- src/components/MentionsLegalesComponent.vue
+++ src/components/MentionsLegalesComponent.vue
@@ -1,27 +1,119 @@
 <template>
-    <h1>Mentions Légales</h1>
-    <!-- ici ta structure Html -->
+<main>
+    <article>
+
+    
+  <p>
+    Conformément aux dispositions des articles 6-III et 19 de la Loi n° 2004-575
+    du 21 juin 2004 pour la Confiance dans l’économie numérique, dite L.C.E.N.,
+    nous portons à la connaissance des utilisateurs et visiteurs du site :
+    www.vvalentin.com les informations suivantes :
+  </p>
+  <h1>Mentions Légales</h1>
+  <p>Statut du propriétaire : <span class="g">Profession Libérale</span></p>
+  <p>
+    Le Propriétaire est :
+    <span class="g"
+      >Véronique Valentin, Psychologue, Psychothérapeute et Coach à Hyères</span
+    >
+  </p>
+  <p>N° Adéli :</p>
+  <p>N° Siret :</p>
+  <p>Adresse postale du propriétaire :</p>
+  <p>Le Responsable de la publication est : Véronique Valentin.</p>
+  <p>Le responsable de la publication est une personne physique.</p>
+  <p>L’hébergeur du site est :</p>
+  <h2>Présentation et principe :</h2>
+  <p>
+    Est désigné ci-après : Utilisateur, tout internaute se connectant et
+    utilisant le site susnommé : www.vvalentin.com.
+  </p>
+  <p>
+    Véronique Valentin s’efforce de fournir sur le site www.vvalentin.com des
+    informations les plus précises possibles (sous réserve de modifications
+    apportées depuis leur mise en ligne), mais ne saurait garantir l’exactitude,
+    la complétude et l’actualité des informations diffusées sur son site,
+    qu’elles soient de son fait ou du fait des tiers partenaires qui lui
+    fournissent ces informations. En conséquence, l’utilisateur reconnaît
+    utiliser ces informations données (à titre indicatif, non exhaustives et
+    susceptibles d’évoluer) sous sa responsabilité exclusive.
+  </p>
+  <h2>Accessibilité</h2>
+  <p>
+    Le site www.vvalentin.com est par principe accessible aux utilisateurs
+    24/24h, 7/7j, sauf interruption, programmée ou non, pour les besoins de sa
+    maintenance ou en cas de force majeure. En cas d’impossibilité d’accès au
+    service, www.vvalentin.com s’engage à faire son maximum afin de rétablir
+    l’accès au service et s’efforcera alors de communiquer préalablement aux
+    utilisateurs les dates et heures de l’intervention. N’étant soumis qu’à une
+    obligation de moyen, www.vvalentin.com ne saurait être tenu pour responsable
+    de tout dommage, quelle qu’en soit la nature, résultant d’une
+    indisponibilité du service.
+  </p>
+  <h2>Propriété intellectuelle :</h2>
+  <p>
+    Véronique Valentin est propriétaire exclusif de tous les droits de propriété
+    intellectuelle ou détient les droits d’usage sur tous les éléments
+    accessibles sur le site, tant sur la structure que sur les textes, images,
+    graphismes, logo, icônes, sons, logiciels. Toute reproduction totale ou
+    partielle du site www.vvalentin.com, représentation, modification,
+    publication, adaptation totale ou partielle de l’un quelconque de ces
+    éléments, quel que soit le moyen ou le procédé utilisé, est interdite, sauf
+    autorisation écrite préalable de Véronique Valentin, à défaut elle sera
+    considérée comme constitutive d’une contrefaçon et passible de poursuite
+    conformément aux dispositions des articles L.335-2 et suivants du Code de
+    Propriété Intellectuelle.
+  </p>
+  <h2>Liens hypertexte et cookies</h2>
+  <p>
+    Le site www.vvalentin.com contient un certain nombre de liens hypertextes
+    vers d’autres sites (partenaires, informations …) mis en place avec
+    l’autorisation de Véronique Valentin. Cependant, Véronique Valentin n’a pas
+    la possibilité de vérifier l’ensemble du contenu des sites ainsi visités et
+    décline donc toute responsabilité de ce fait quand aux risques éventuels de
+    contenus illicites.
+  </p>
+  <p>
+    L’utilisateur est informé que lors de ses visites sur le site
+    www.vvalentin.com, un ou des cookies sont susceptibles de s’installer
+    automatiquement sur son ordinateur par l’intermédiaire de son logiciel de
+    navigation. Un cookie est un bloc de données qui ne permet pas d’identifier
+    l’utilisateur, mais qui enregistre des informations relatives à la
+    navigation de celui-ci sur le site.
+  </p>
+  <p>
+    Le paramétrage du logiciel de navigation permet d’informer de la présence de
+    cookie et éventuellement, de la refuser de la manière décrite à l’adresse
+    suivante :
+    <a href="http://www.cnil.fr" target="_blank">www.cnil.fr.</a> L’utilisateur
+    peut toutefois configurer le navigateur de son ordinateur pour refuser
+    l’installation des cookies, sachant que le refus d’installation d’un cookie
+    peut entraîner l’impossibilité d’accéder à certains services. Pour tout
+    blocage des cookies, tapez dans votre moteur de recherche : blocage des
+    cookies sous le nom de votre navigateur (Internet Explorer, Firefox, Chrome,
+    Safari, Opéra, etc..) et suivez les instructions en fonction de votre
+    version.
+  </p>
+  </article>
+</main>
+  <!-- ici ta structure Html -->
 </template>
 
 <script>
 export default {
-  name: 'MentionsLegalesComponent', /*
+  name: "MentionsLegalesComponent" /*
   le nom du composant
   dans le html du parent il se "source" de la maniere suivante : <mentionslegales-component>
   dans le scripting (JS) du parent il se declare avec MentionsLegalesComponent et doit etre importé
-  */
+  */,
   props: {
-      variable1: String // type de variable (string,integer,...)
+   
   },
-  data() {
-      return {
-          variable2: "ceci est une variable du composant (passable aux composants enfants)"
-      }
-  } 
-}
+  data() {},
+};
 </script>
 
 <style>
-    /* ici le css */
+/* ici le css */
 </style>
 
