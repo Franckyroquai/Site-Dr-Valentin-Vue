6a049ed4590587d8bac7c921e25788e3298304f1
Franckyroquai <franckyroquai@gmail.com>
2022-03-11 20:30:29 +0100

    ajout bouton déconnexion

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,9 +1,8 @@
 <template>
-     <!-- Barre de navigation-->
-
     <nav class="navbar sticky-top shadow-sm navbar-expand-lg navbar-light bg-light style=background-color: #e3f2fd;">
   <div class="container-fluid">
     <router-link to="/" class="navbar-brand">V. Valentin</router-link>
+    <button v-if="isLogged()" @click="disconnect">deconnexion</button>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -29,7 +28,6 @@
             Méthodes
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <!-- <li><router-link to="/approche-therapeutique" class="dropdown-item">Approche Thérapeutique</router-link></li> -->
             <li><router-link to="/approche-systemique" class="dropdown-item">Approche Systémique</router-link></li>
             <li><router-link to="/emdr" class="dropdown-item">EMDR</router-link></li>
             <li><router-link to="/brainspotting" class="dropdown-item">Brainspotting</router-link></li>
@@ -59,78 +57,27 @@
     </div>
   </div>
 </nav>
-     
-    <!-- <div class="navbar">
-      <router-link to="/">Accueil</router-link>
-      <router-link to="/about">A Propos</router-link>
-      <router-link to="/approche-systemique">Approche Systémique</router-link>
-      <router-link to="/approche-therapeutique">Approche Thérapeutique</router-link>
-      <router-link to="/emdr">EMDR</router-link>
-      <router-link to="/brainspotting">Brainspotting</router-link>
-      <router-link to="/therapie-enfant">Thérapie de l'Enfant</router-link>
-      <router-link to="/therapie-adolescent">Thérapie de l'Adolescent</router-link>
-      <router-link to="/therapie-adulte">Thérapie de l'Adulte</router-link>
-      <router-link to="/therapie-familiale">Thérapie Familiale</router-link>
-      <router-link to="/therapie-de-couple">Thérapie de couple</router-link>
-      <router-link to="/therapie-psychocorporelle">Thérapie PsychoCorporelle</router-link>
-      <router-link to="/points-ethiques">Points Ethiques</router-link>
-      <router-link to="/liens-videos">Liens Vidéos</router-link>
-      <router-link to="/diplomes">Diplômes et Certifications</router-link>
-      <router-link to="/modalites-pratiques">Modalités Pratiques</router-link>
-      <router-link to="/contact">Contact</router-link>
-      
-
-      <link-component Link="wwww.google.fr" linkName="le nom du lien"></link-component>
-   </div> -->
-   <!-- <router-view></router-view> -->
 </template>
 
 <script>
-// import LinkComponent from './LinkComponent.vue'
 export default {
   name: 'NavbarComponent',
-   /*
-  le nom du composant
-  dans le html du parent il se "source" de la maniere suivante : <default-component>
-  dans le scripting (JS) du parent il se declare avec DefaultComponent et doit etre importé
-  */
- components: {
-    //  LinkComponent
- },
-  props: {
-      variable1: String // type de variable (string,integer,...)
-  },
   data() {
       return {
-          variable2: "ceci est une variable du composant (passable aux composants enfants)"
       }
-  } 
+  },
+  methods: {
+    disconnect() {
+      return true;
+    },
+    isLogged() {
+      return false;
+    }
+  },
 }
 </script>
 
 <style>
-    /* .navbar{ position: relative;
-    top: 0;
-    left: 0;
-    z-index: 10;
-    width: 100%;
-    height: 6em;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    display: flex;
-    justify-content:space-around;
-    align-items: center;
-    border: 3px solid rgb(248, 234, 248);
-    border-radius:10px;
-    padding: 20px;
-    background-color:rgb(211, 211, 211);
-} */
-/* .navbar-nav > li{
-  
-  margin-left:25px;
-  margin-right:25px;
-} */
 
 </style>
 
