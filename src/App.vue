503853bda02c7ca75d73ef9bdf0d3ab49da37668
Pierre POMET <pierre.pomet.pro@gmail.com>
2022-03-11 16:04:35 +0100

    MOD: APP > dev

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
   <!-- <HeaderComponent></HeaderComponent> -->
-  <Navbar-Component></Navbar-Component>  
+  <Navbar-Component></Navbar-Component>
+  <!-- <quotes-component></quotes-component> -->
   <router-view></router-view>
   <footer-component></footer-component>
+  <!-- TODO: je crois que pour ton footer il faut mettre ici un filler -->
 </template>
 
 <script>
 // import HeaderComponent from './components/HeaderComponent.vue'
-import NavbarComponent from './components/NavbarComponent.vue'
-import FooterComponent from './components/FooterComponent.vue'
-
+import NavbarComponent from "./components/NavbarComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
+// import QuotesComponent from "./components/QuotesComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     //HeaderComponent,
     NavbarComponent,
     FooterComponent,
+    // QuotesComponent,
     // CarouselComponent,
     // AccueilComponent,
-  }
-}
+  },
+};
 </script>
 
 <style>
