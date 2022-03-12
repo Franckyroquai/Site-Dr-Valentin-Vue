1240cde6a8cd6147c9367c960be296d8366e65f4
Franckyroquai <franckyroquai@gmail.com>
2022-03-12 20:10:04 +0100

    workflow console.log

--- src/components/RegisterComponent.vue
+++ src/components/RegisterComponent.vue
@@ -20,42 +20,45 @@
   </div>
   <button type="submit" class="btn btn-primary">Envoyer</button>
 </form> -->
-<main>
-  <h2>Créez un compte</h2>
-  <div class="root">
-    
-    <p>
-      <input type="text" placeholder="Email" v-model="state.email" />
-      <span v-if="v$.email.$error">
-        {{ v$.email.$errors[0].$message }}
-      </span>
-    </p>
-    <p>
-      <input type="password" placeholder="Password" v-model="state.password.value" />
-      <span v-if="v$.password.value.$error">
-        {{ v$.password.value.$errors[0].$message }}
-      </span>
-    </p>
-    <p>
-      <input
-        type="password"
-        placeholder="Confirm Password"
-        v-model="state.password.confirm"
-      />
-      <span v-if="v$.password.confirm.$error">
-        {{ v$.password.confirm.$errors[0].$message }}
-      </span>
-    </p>
-    <button @click="submitForm">Envoyer</button>
-  </div>
-</main>
+  <main>
+    <h2>Créez un compte</h2>
+    <div class="root">
+      <p>
+        <input type="text" placeholder="Email" v-model="state.email" />
+        <span v-if="v$.email.$error">
+          {{ v$.email.$errors[0].$message }}
+        </span>
+      </p>
+      <p>
+        <input
+          type="password"
+          placeholder="Password"
+          v-model="state.password.value"
+        />
+        <span v-if="v$.password.value.$error">
+          {{ v$.password.value.$errors[0].$message }}
+        </span>
+      </p>
+      <p>
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          v-model="state.password.confirm"
+        />
+        <span v-if="v$.password.confirm.$error">
+          {{ v$.password.confirm.$errors[0].$message }}
+        </span>
+      </p>
+      <button @click="submitForm">Envoyer</button>
+    </div>
+  </main>
 </template>
 
 <script>
 import useValidate from "@vuelidate/core";
 import { reactive, computed } from "vue";
 import { required, email, minLength, sameAs } from "@vuelidate/validators";
-import axios from "axios";
+import { publicRequest } from "../services/requester";
 
 export default {
   name: "RegisterComponent",
@@ -69,43 +72,45 @@ export default {
     });
     const rules = computed(() => {
       return {
-      email: { required, email },
-      password: {
-        value: { required, minLength: minLength(6) },
-        confirm: { required, sameAs: sameAs(state.password.value) },
-      }
-      }});
+        email: { required, email },
+        password: {
+          value: { required, minLength: minLength(6) },
+          confirm: { required, sameAs: sameAs(state.password.value) },
+        },
+      };
+    });
 
-      const v$ = useValidate(rules, state)
-      
-      return { state, v$ };
+    const v$ = useValidate(rules, state);
+
+    return { state, v$ };
   },
-  
+
   methods: {
     async submitForm() {
       this.v$.$validate(); // vérifie les inputs
       if (!this.v$.$error) {
         try {
-
-        const httpResponse = await axios.post(
-          "http://127.0.0.1:3000/signup", 
-          {
-            email: this.state.email, 
-            password: this.state.password.value
-          })
+          console.log("hoy bobby");
+          const httpResponse = await publicRequest("/register", "post", {
+            email: this.state.email,
+            password: this.state.password.value,
+          });
+          console.log("-------------------------------------")
           console.log(httpResponse.status);
           if (httpResponse.status === 200) {
-            alert("vous avez bien ete enregistre.\nVous allez etre redirige vers la page de login")
+            alert(
+              "vous avez bien ete enregistre.\nVous allez etre redirige vers la page de login"
+            );
             this.$router.push("/login");
-          } 
+          }
         } catch (error) {
           console.error(error);
-          alert ("does not compute, Error, Error, ERRR.&%^&$");
+          alert("does not compute, Error, Error, ERRR.&%^&$");
         }
       } else {
         console.info("password: ", this.state.password.value);
         console.info("confirm : ", this.state.password.confirm);
-        this.v$.$errors.forEach(error => console.warn(error.$message));
+        this.v$.$errors.forEach((error) => console.warn(error.$message));
         console.error("Form failed validation");
       }
     },
