326cd1cfde66c82aa85e5cb175cd1d1a44d1d03a
Franckyroquai <franckyroquai@gmail.com>
2022-03-20 20:46:52 +0100

    ajout de style

--- src/components/Diplomes.vue
+++ src/components/Diplomes.vue
@@ -3,31 +3,44 @@
     <h1 class="is">Diplômes et Certifications</h1>
     <br />
     <article>
-      <p>Master2 -Psychologie Clinique et psychopathologie (Université Aix-Marseille)</p>
+      <section class="premier">
+        <article class="flex-item img w40">
+        <img
+          src="../assets/Images/diplome.jpg"
+          class="hero border15"
+          alt="image de diplome et chapeau d'étudiant"
+        />
+      </article>
+      <article class="flex-item w50">
+        <ul class="bord list">
+      <li>Master2 -Psychologie Clinique et psychopathologie (Université Aix-Marseille)</li>
 
-      <p>
+      <li>
         Formation psychanalytique (Freud, Jung, Lacan ) et psychanalyse didactique de 9
         ans (Avignon)
-      </p>
-      <p>CERTIFICAT de praticien en thérapie psychocorporelle et relaxation (INFIPP)</p>
-      <p>CERTIFICAT Européen de thérapeute familial (Réseau /Famille)</p>
-      <p>
+      </li>
+      <li>CERTIFICAT de praticien en thérapie psychocorporelle et relaxation (INFIPP)</li>
+      <li>CERTIFICAT Européen de thérapeute familial (Réseau /Famille)</li>
+      <li>
         CERTIFICAT Européen de praticien <router-link to="/emdr">EMDR</router-link> (EMDR France)<br />
         + Dissociation et traumas complexes (H. Dellucci)<br />
         + Traitement des phobies par l’<router-link to="/emdr">EMDR</router-link> (Ad. De Jong)
-      </p>
-      <p>CERTIFICATION <router-link to="/brainspotting">Brainspotting</router-link> niv 1-2-3 (Grand D.)</p>
-      <p>
+      </li>
+      <li>CERTIFICATION <router-link to="/brainspotting">Brainspotting</router-link> niv 1-2-3 (Grand D.)</li>
+      <li>
         CERTIFICATS : <br />
         - Thérapie par l’image (IMTT)-IETSP Paris <br />
         -TIPI ( Marseille) <br />
         -Méthode des 2 points (R.Bartlett-F.Kinslow) <br />
         - Langue des Signes (IVT Paris)
-      </p>
+      </li>
+        </ul>
+      </article>
+      </section>
       <h2 class="s">Orientation</h2>
       <p>• Approche humaniste, intégrative, systémique.</p>
       <h2 class="s">Pour Quel public ?</h2>
-      <ul class="bord">
+      <ul class="bord list">
       <li class="s">Enfants / Adolescents, Adultes / séniors, Couples / familles</li>
       <li><span class="s">Sportifs de haut niveau :</span> Préparation mentale</li>
       <li>
