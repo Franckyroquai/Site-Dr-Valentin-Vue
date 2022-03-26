d7574dbb86bddb65cb8b486669794252714e98fa
Franckyroquai <franckyroquai@gmail.com>
2022-03-26 23:27:43 +0100

    chgt nom classes italic bold underline

--- src/components/ModalitesPratiques.vue
+++ src/components/ModalitesPratiques.vue
@@ -1,94 +1,91 @@
 <template>
-<h1 class="hero2 i">Modalités pratiques</h1>
-    <main>
-        <br>
-        <article>
-        <h2 class="i">
-          Consultations
-        </h2>
-        <br>
-        <p class="bg1">Elles durent environ 60 minutes
-          <br>(parfois plus avec l’EMDR ou le Brainspotting).
-        </p> 
-        <br>
-        <h2 class="i">
-          Honoraires
-        </h2>
-        <br>
-        <section class="bg1">
-        <p>
-         Séance individuelle d’une heure environ (adulte) : 70 euros 
-        </p>
-        <p>
-          Séance individuelle de 30 à 45 minutes (enfant) : 50 euros
-        </p>
+  <h1 class="hero2 italic">Modalités pratiques</h1>
+  <main>
+    <br />
+    <article>
+      <h2 class="italic">Consultations</h2>
+      <br />
+      <p class="bg1">
+        Elles durent environ 60 minutes <br />(parfois plus avec l’EMDR ou le
+        Brainspotting).
+      </p>
+      <br />
+      <h2 class="italic">Honoraires</h2>
+      <br />
+      <section class="bg1">
+        <p>Séance individuelle d’une heure environ (adulte) : 70 euros</p>
+        <p>Séance individuelle de 30 à 45 minutes (enfant) : 50 euros</p>
         <p>
           Séance d’une heure 15 ou 30 (couples / famille) : de 90 à 120 euros
         </p>
-        </section>
-        <br>
-          <section class="bg1">
+      </section>
+      <br />
+      <section class="bg1">
         <p>
-         Le règlement des honoraires s’effectue <span class="g">à la fin de chaque séance</span>, par <span class="g">chèque</span> ou par <span class="g">espèces</span>, par <span class="g">virement</span> pour les consultations en ligne.
-        </p>
-        <p>
-          Dans certaines situations, étudiants, personnes en situation de handicap, demandeurs d’emploi, les tarifs pourront être adaptés. Nous verrons cela ensemble. 
-        </p>
-        <p>
-         Les séances chez le psychologue ne sont pas prises en charge par la Sécurité Sociale. 
+          Le règlement des honoraires s’effectue
+          <span class="bold">à la fin de chaque séance</span>, par
+          <span class="bold">chèque</span> ou par
+          <span class="bold">espèces</span>, par
+          <span class="bold">virement</span> pour les consultations en ligne.
         </p>
         <p>
-         Par contre, votre mutuelle rembourse peut-être une partie des consultations, souvent sous forme de forfait. Pensez à la contacter.  
+          Dans certaines situations, étudiants, personnes en situation de
+          handicap, demandeurs d’emploi, les tarifs pourront être adaptés. Nous
+          verrons cela ensemble.
         </p>
-        <p class="g">
-          Un rendez-vous non annulé 48 heures avant, sera dû.
+        <p>
+          Les séances chez le psychologue ne sont pas prises en charge par la
+          Sécurité Sociale.
         </p>
-          </section>
-        </article>
-        <br>
-        <h2 class="i">
-          Consultations à distance
-        </h2>
-        <br>
-        <section class="premier">
+        <p>
+          Par contre, votre mutuelle rembourse peut-être une partie des
+          consultations, souvent sous forme de forfait. Pensez à la contacter.
+        </p>
+        <p class="bold">Un rendez-vous non annulé 48 heures avant, sera dû.</p>
+      </section>
+    </article>
+    <br />
+    <h2 class="italic">Consultations à distance</h2>
+    <br />
+    <section class="premier">
       <article class="flex-item w50 bg1">
         <p>
-          Les consultations peuvent également se faire en VISIO (en passant par Gmail, Zoom, Skype…).
+          Les consultations peuvent également se faire en VISIO (en passant par
+          Gmail, Zoom, Skype…).
         </p>
         <p>
-          La consultation à distance a l’avantage de rendre possible un travail thérapeutique quelle que soit la situation.
+          La consultation à distance a l’avantage de rendre possible un travail
+          thérapeutique quelle que soit la situation.
         </p>
         <p>
-          Je vous invite à vous organiser pour ne pas être dérangé(e) pendant la séance (téléphone, visites) et prévoyez de vous installer dans un endroit où vous êtes à l'aise.
+          Je vous invite à vous organiser pour ne pas être dérangé(e) pendant la
+          séance (téléphone, visites) et prévoyez de vous installer dans un
+          endroit où vous êtes à l'aise.
         </p>
-          </article>
-           <article class="flex-item img w40">
+      </article>
+      <article class="flex-item img w40">
         <img
           src="../../public/assets/Images/teleconsultation.jpg"
           class="hero"
           alt="dessin de femmes communiquant au travers d'un écran"
         />
       </article>
-
-        </section>
-    
-    
-      </main>
+    </section>
+  </main>
 </template>
 
 <script>
 export default {
-  name: 'ModalitesPratiquesComponent', 
+  name: "ModalitesPratiquesComponent",
   props: {
-      variable1: String // type de variable (string,integer,...)
+    variable1: String, // type de variable (string,integer,...)
   },
   data() {
-      return {}
-  } 
-}
+    return {};
+  },
+};
 </script>
 
 <style>
-  
 </style>
 
