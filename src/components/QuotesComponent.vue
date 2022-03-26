06366f9dc2f3eaa06a8ff71d3c5535cda3242654
Franckyroquai <franckyroquai@gmail.com>
2022-03-26 21:03:29 +0100

    chgt titre de l'onglet (name)

--- src/components/QuotesComponent.vue
+++ src/components/QuotesComponent.vue
@@ -1,30 +1,45 @@
 <template>
+<h1 class="hero2 i">Gestion des citations</h1>
   <main>
-    <h1>Nombre de citations: {{ count }}</h1>
-    <button @click="flushQuotes">Effacer toutes les citations</button>
-    <h3>Liste des Citation:</h3>
-    <p v-if="!list.length">non chargées</p>
-    <button v-if="!list.length" @click="getListOfQuotes">
-      Charger la liste des citations
+    <h1>Nombre : {{ count }}</h1>
+    <br>
+    <h3>Liste :</h3>
+    <p class="i" v-if="!list.length">Cliquez sur le bouton pour accéder à la liste</p>
+    <button class="btn btn-info i" v-if="!list.length" @click="getListOfQuotes">
+      Afficher les citations
     </button>
-    <button v-if="list.length" @click="list = []">Masquer la liste</button>
-    <ul>
+    <br>
+    <button class="btn btn-info" v-if="list.length" @click="list = []">Masquer la liste</button>
+    <br><br>
+    <ul class="list">
       <li v-for="elem in list" :key="elem">
         <div id="quoteContainer">
-          <h6>auteur: {{ elem.author }}</h6>
-          <p>
-            {{ elem.text }}
-          </p>
-          <button @click="deleteQuoteInList(elem._id, elem.author)">del</button>
+          <h5 class="g" >Auteur : {{ elem.author }}</h5>
+          <p class="i">"{{ elem.text }}"</p>
+          <button class="btn btn-success" @click="deleteQuoteInList(elem._id, elem.author)">Supprimer la citation</button>
+          <br><br>
         </div>
       </li>
     </ul>
-    <h3>Créer une nouvelle citation:</h3>
-    <form>
-      <input v-model="author" placeholder="Auteur" />
-      <textarea v-model="text" placeholder="Citation" />
+    <br>
+    <h3>Enregistrer une nouvelle citation dans la liste :</h3>
+    <div class="container">
+             <form>
+                 <div class="input-group mb-3">
+                           <div class="input-group-prepend"><span class="input-group-text" aria-label="autheur">Autheur</span></div>
+      <input type="text" class="form-control" v-model="author" placeholder="Auteur" />
+                 </div>
+                 <br>
+<div class="input-group mb-3">
+      <textarea class="form-control" v-model="text" placeholder="Citation" />
+</div>
     </form>
-    <button @click="sendQuote">Envoyer la citation</button>
+    </div>
+    <br>
+    <button class="btn btn-primary" @click="sendQuote">Envoyer la citation</button>
+    <br><br><br>
+    <!-- <button class="btn btn-danger" @click="flushQuotes">Effacer toutes les citations</button> -->
+    <br><br>
   </main>
 </template>
 
