54e2ebcc60a98e2fb05986f55456570f4faafe81
Franckyroquai <franckyroquai@gmail.com>
2022-03-26 23:27:57 +0100

    chgt nom classes italic bold underline

--- src/components/PointsEthiques.vue
+++ src/components/PointsEthiques.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="hero2 i">Les points-clés de ma pratique</h1>
+  <h1 class="hero2 italic">Les points-clés de ma pratique</h1>
   <main>
     <br />
     <br />
@@ -13,7 +13,7 @@
       </article>
       <article class="flex-item w60 bg1">
         <p>
-          Un <span class="g">espace de créativité et de confiance,</span> où
+          Un <span class="bold">espace de créativité et de confiance,</span> où
           vous pouvez vous raconter et vous projeter, en y étant reconnu(e) pour
           vous-même et non jugé(e).
         </p>
@@ -24,7 +24,7 @@
           peuvent en freiner le plein accomplissement.
         </p>
         <p>
-          Une <span class="g">déontologie rigoureuse</span> garantissant la
+          Une <span class="bold">déontologie rigoureuse</span> garantissant la
           confidentialité et le respect de la personne.
         </p>
       </article>
@@ -32,43 +32,44 @@
     <br />
     <br />
     <article>
-      <h2 class="ig">Quelques éléments de notre code de déontologie :</h2>
+      <h2 class="italic bold">Quelques éléments de notre code de déontologie :</h2>
       <br />
       <br />
       <ul class="list bg1">
         <li>
-          <span class="igs">Pratique d’accompagnement</span> : Le thérapeute
+          <span class="italic bold underline">Pratique d’accompagnement</span> : Le thérapeute
           s’autorise en conscience à exercer cette fonction à partir de son
           parcours de formation, d’une expérience professionnelle enrichie de sa
           formation personnelle.
         </li>
         <li>
-          <span class="igs">Confidentialité</span> : Le thérapeute s’astreint au
-          <span class="ig">secret professionnel</span>.
+          <span class="italic bold underline">Confidentialité</span> : Le thérapeute s’astreint au
+          <span class="italic bold underline">secret professionnel</span>.
         </li>
         <li>
-          <span class="igs">Supervision établie</span> : Il est tenu de disposer
+          <span class="italic bold underline">Supervision établie</span> : Il est tenu de disposer
           d’un lieu de supervision par un pair ou un tiers compétent.
         </li>
         <li>
-          <span class="igs">Intégrité et probité</span> : Il a pour obligation
+          <span class="italic bold underline">Intégrité et probité</span> : Il a pour obligation
           de ne pas exploiter une relation professionnelle à des fins
           personnelles, religieuses, sectaires, politiques, ou en vue de tout
           autre intérêt idéologique.
         </li>
         <li>
-          <span class="igs">Responsabilité des décisions</span> : La personne
+          <span class="italic bold underline">Responsabilité des décisions</span> : La personne
           reste maitre d’œuvre dans le processus. Elle en a la responsabilité et
           reste libre de ses décisions et actions.
         </li>
       </ul>
       <br />
-      <h2 class="i">
+      <h2 class="italic">
         "C’est sur soi même qu’il faut œuvrer,<br />c’est en soi-même qu’il faut
         chercher"
       </h2>
       <h3 class="author">Paracelse</h3>
     </article>
+    <br>
   </main>
 </template>
 
