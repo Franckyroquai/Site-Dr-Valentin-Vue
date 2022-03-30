6746eeaad4df4676280ec30e0624a1cbcf088aa2
Franckyroquai <franckyroquai@gmail.com>
2022-03-30 11:59:06 +0200

    correction bug affichage par encapsulage dans div

--- src/components/TherapieEnfant.vue
+++ src/components/TherapieEnfant.vue
@@ -1,77 +1,76 @@
 <template>
-  <h1 class="hero2 italic">La Thérapie de l’enfant</h1>
-  <main>
-    <br />
-    <h2 class="italic">
-      "Vous lui demandez de se calmer mais cette crise est sa manière de se
-      calmer"
-    </h2>
-    <h3 class="author">Isabelle FILLIOSAT</h3>
-    <br />
-    <img
-      src="../../public/assets/Images/enfant2.jpg"
-      width="300"
-      alt="Petite fille escaladant une montagne tend la main vers une main adulte"
-    />
-    <article>
+  <div>
+    <h1 class="hero2 italic">La Thérapie de l’enfant</h1>
+    <main>
       <br />
-      <section class="bg1">
-        <p>Grandir, nous le savons tous, ne va pas de soi.</p>
-        <p>
-          Si l’enfant fait preuve d’une grande adaptabilité face aux aléas de la
-          vie, lorsqu’il traverse une épreuve trop lourde pour ses petites
-          épaules, une réelle souffrance peut s’installer et s’aggraver si elle
-          n’est pas entendue … au bon endroit.
+      <h2 class="italic">
+        "Vous lui demandez de se calmer mais cette crise est sa manière de se
+        calmer"
+      </h2>
+      <h3 class="author">Isabelle FILLIOSAT</h3>
+      <br />
+      <img
+        src="../../public/assets/Images/enfant2.jpg"
+        width="300"
+        alt="Petite fille escaladant une montagne tend la main vers une main adulte"
+      />
+      <article>
+        <br />
+        <section class="bg1">
+          <p>Grandir, nous le savons tous, ne va pas de soi.</p>
+          <p>
+            Si l’enfant fait preuve d’une grande adaptabilité face aux aléas de la
+            vie, lorsqu’il traverse une épreuve trop lourde pour ses petites
+            épaules, une réelle souffrance peut s’installer et s’aggraver si elle
+            n’est pas entendue … au bon endroit.
+          </p>
+          <p>
+            Dès lors qu’une difficulté est ressentie par l’enfant et/ou les
+            parents, tout peut être motif à consulter. Et ces comportements,
+            sources d’inquiétude, sont des plus variés :
+          </p>
+        </section>
+        <section class="premier">
+          <article class="flex-item w50">
+            <ul class="bg1 list italic">
+              <li>Trouble du sommeil, réveils nocturnes</li>
+              <li>Enurésie, encopresie</li>
+              <li>Anxiété, angoisse de séparation</li>
+              <li>Agressivité, inhibition, harcèlement, isolement</li>
+              <li>Refus de grandir</li>
+              <li>Difficultés scolaires, trouble des apprentissages</li>
+              <li>Hyperactivité, trouble de l’attention</li>
+              <li>Hypersensibilité</li>
+              <li>Problème d’estime de soi</li>
+              <li>Auto ou hétero agressivité</li>
+              <li>Somatisations répétées</li>
+              <li>Arrivée d’un petit frère ou d’une petite sœur</li>
+              <li>Difficultés dans la relation parents/enfant</li>
+              <li>Divorce, décès d’un parent, familles recomposées…</li>
+            </ul>
+          </article>
+          <article class="flex-item img w40">
+            <img
+              src="../../public/assets/Images/pierres-zen.jpg"
+              class="hero"
+              alt="galets empliés sur du sable"
+            />
+          </article>
+        </section>
+        <p class="bg1">
+          Après évaluation, les séances suivantes pourront déboucher (ou pas) sur
+          un accompagnement familial, ou un coaching parental « step by step »,
+          selon l’évolution des séances avec l’enfant.
         </p>
-        <p>
-          Dès lors qu’une difficulté est ressentie par l’enfant et/ou les
-          parents, tout peut être motif à consulter. Et ces comportements,
-          sources d’inquiétude, sont des plus variés :
-        </p>
-      </section>
-      <section class="premier">
-        <article class="flex-item w50">
-          <ul class="bg1 list italic">
-            <li>Trouble du sommeil, réveils nocturnes</li>
-            <li>Enurésie, encopresie</li>
-            <li>Anxiété, angoisse de séparation</li>
-            <li>Agressivité, inhibition, harcèlement, isolement</li>
-            <li>Refus de grandir</li>
-            <li>Difficultés scolaires, trouble des apprentissages</li>
-            <li>Hyperactivité, trouble de l’attention</li>
-            <li>Hypersensibilité</li>
-            <li>Problème d’estime de soi</li>
-            <li>Auto ou hétero agressivité</li>
-            <li>Somatisations répétées</li>
-            <li>Arrivée d’un petit frère ou d’une petite sœur</li>
-            <li>Difficultés dans la relation parents/enfant</li>
-            <li>Divorce, décès d’un parent, familles recomposées…</li>
-          </ul>
-        </article>
-        <article class="flex-item img w40">
-          <img
-            src="../../public/assets/Images/pierres-zen.jpg"
-            class="hero"
-            alt="galets empliés sur du sable"
-          />
-        </article>
-      </section>
-      <p class="bg1">
-        Après évaluation, les séances suivantes pourront déboucher (ou pas) sur
-        un accompagnement familial, ou un coaching parental « step by step »,
-        selon l’évolution des séances avec l’enfant.
-      </p>
-    </article>
-  </main>
+      </article>
+    </main>
+  </div>
 </template>
 
 <script>
 export default {
   name: "TherapieEnfantComponent",
   
-  data() {
-    return {};
-  },
 };
 </script>
 
