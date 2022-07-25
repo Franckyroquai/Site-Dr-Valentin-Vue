e49b423881610627b85e951a45ba96680b3213c2
Franckyroquai <franckyroquai@gmail.com>
2022-07-25 21:29:22 +0200

    maj commentaire image

--- src/components/MentionsLegalesComponent.vue
+++ src/components/MentionsLegalesComponent.vue
@@ -15,16 +15,16 @@
         En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site internet <span class="bold">https://www.veroniquevalentin.fr</span> l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi:
       </p>
       <p>
-        <span class="bold">Propriétaire du site :</span> V.Valentin PSY CONSEIL - Contact : VValentin.pro@gmail.com - <span style="font-family: arial, sans-serif"> 06 86 97 86 13</span> - Adresse : 7 rue Castueil 83400 Hyères.
+        <span class="bold">Propriétaire du site :</span> V.Valentin PSY CONSEIL - Contact : VValentin.pro@gmail.com - <span style="font-family: arial, sans-serif"> 06 86 97 86 13</span> - Adresse : <span style="font-family: arial, sans-serif">7</span> rue Castueil <span style="font-family: arial, sans-serif">83400</span> Hyères.
       </p>
       <p>
-        <span class="bold">Identification de l'entreprise : </span>SASU V.Valentin PSY CONSEIL au capital social de 3000€ - SIREN : 801836248 - RCS : Toulon B 801 836 248 - Numéro Individuel TVA : FR 33 801836248 - Adresse postale : 7 rue Castueil 83400 Hyères
+        <span class="bold">Identification de l'entreprise : </span>SASU V.Valentin PSY CONSEIL au capital social de <span style="font-family: arial, sans-serif">3000 €</span> - SIREN : <span style="font-family: arial, sans-serif">801836248</span> - RCS : Toulon B <span style="font-family: arial, sans-serif">801 836 248</span> - Numéro Individuel TVA : FR <span style="font-family: arial, sans-serif">33 801836248</span> - Adresse postale : <span style="font-family: arial, sans-serif">7</span> rue Castueil <span style="font-family: arial, sans-serif">83400</span> Hyères
       </p>
       <p>
-        <span class="bold">Directeur de la publication :</span> Véronique Valentin - Contact: 06 86 97 86 13
+        <span class="bold">Directeur de la publication :</span> Véronique Valentin - Contact: <span style="font-family: arial, sans-serif">06 86 97 86 13</span>
       </p>
       <p>
-        <span class="bold">Hébergeur :</span> OVH SAS - 2 rue Kellermann - BP 80157 - 59053 Roubaix Cedex 1 - Téléphone : 1007 
+        <span class="bold">Hébergeur :</span> OVH SAS - <span style="font-family: arial, sans-serif">2</span> rue Kellermann - BP <span style="font-family: arial, sans-serif">80157</span> - <span style="font-family: arial, sans-serif">59053</span> Roubaix Cedex <span style="font-family: arial, sans-serif">1</span> - Téléphone : <span style="font-family: arial, sans-serif">1007</span> 
       </p>
       <h2 class="underline">
         Propriété intellectuelle et contrefaçons
@@ -36,7 +36,7 @@
         Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de l’entreprise <span class="bold">V.Valentin PSY CONSEIL</span>.
       </p>
       <p>
-        Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient sera considérée comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.
+        Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient sera considérée comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.<span style="font-family: arial, sans-serif">335-2</span> et suivants du Code de Propriété Intellectuelle.
       </p>
        <h2 class="underline">
         Limitations de responsabilité 
@@ -51,22 +51,10 @@
         <span class="bold">V.Valentin PSY CONSEIL</span> s’engage à sécuriser au mieux le site <span class="bold">https://www.veroniquevalentin.fr</span>, cependant sa responsabilité ne pourra être mise en cause si des données indésirables sont importées et installées sur son site à son insu.
       </p>
       <h2 class="underline">
-        Liens hypertextes et cookies
+        Liens hypertextes
       </h2>
       <p>
         Le site <span class="bold">https://www.veroniquevalentin.fr</span> contient des liens hypertextes vers d’autres sites et dégage toute responsabilité à propos de ces liens externes ou des liens créés par d’autres sites vers <span class="bold">https://www.veroniquevalentin.fr</span>.
-      </p>
-      <p>
-        La navigation sur le site <span class="bold">https://www.veroniquevalentin.fr</span> est susceptible de provoquer l’installation de cookie(s) sur l’ordinateur de l’utilisateur.
-      </p>
-      <p>
-        Un "cookie" est un fichier de petite taille qui enregistre des informations relatives à la navigation d’un utilisateur sur un site. Les données ainsi obtenues permettent d'obtenir des mesures de fréquentation, par exemple.
-      </p>
-      <p>
-        Vous avez la possibilité <span class="bold">d’accepter ou de refuser les cookies</span> en modifiant les paramètres de votre navigateur. Aucun cookie ne sera déposé sans votre consentement.
-      </p>
-      <p>
-        Les cookies sont enregistrés pour une durée maximale de 1 mois.
       </p>
       <h2 class="underline">
         Flux RSS
